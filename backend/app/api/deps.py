2beaaeaef272b772d083dcda57b36171c2c24d1c
Dmitry <dodegov@smarthomecrafters.ai>
2025-04-13 19:48:19 +0300

    Добавлены dependencies

---
+++ backend/app/api/deps.py
@@ -0,0 +1,12 @@
+from fastapi import Depends
+from typing import Annotated
+
+from app.db.db_manager import DBManager
+from app.db.init_db import async_session_maker
+
+
+async def get_db():
+    async with DBManager(session_factory=async_session_maker) as db:
+        yield db
+
+DBDep = Annotated[DBManager, Depends(get_db)]
