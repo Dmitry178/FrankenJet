b425df9d22ee3a3656cb982bacc1a94635eefeab
Dmitry <dodegov@smarthomecrafters.ai>
2025-04-13 19:49:59 +0300

    Добавлен роут для редиректа на swagger ui в локальном режиме работы приложения

---
+++ backend/app/api/routes/local.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter
+from starlette.responses import RedirectResponse
+
+from app.core.config import settings, AppMode
+
+index_local_router = APIRouter(tags=["Local"])
+
+
+@index_local_router.get("/", include_in_schema=False, summary="Редирект на Swagger UI")
+async def index():
+    """
+    Перенаправление на Swagger UI в локальном режиме работы приложения
+    """
+
+    if settings.APP_MODE != AppMode.local:
+        return {"detail": "Not Found"}
+
+    return RedirectResponse("/docs")
