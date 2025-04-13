38b75b38657b610427299887c5127e42bd9a3b9c
Dmitry <dodegov@smarthomecrafters.ai>
2025-04-13 19:47:57 +0300

    Добавлена инициализация логов

---
+++ backend/app/core/logs.py
@@ -0,0 +1,11 @@
+import logging
+import sys
+
+from app.core.config import settings
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[logging.StreamHandler(sys.stdout)]
+)
+logger = logging.getLogger(settings.APP_NAME)
