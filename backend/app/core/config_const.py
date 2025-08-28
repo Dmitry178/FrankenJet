a296098927ecd2ebc8af76fcbda312693287bd49
Dmitry <dodegov@smarthomecrafters.ai>
2025-08-28 19:14:39 +0300

    Добавлены константы для настроек приложения

---
+++ backend/app/core/config_const.py
@@ -0,0 +1,3 @@
+OAUTH2_GOOGLE_URL = "https://accounts.google.com/o/oauth2/v2/auth"
+OAUTH2_GOOGLE_TOKEN_URL = "https://oauth2.googleapis.com/token"
+OAUTH2_GOOGLE_REDIRECT_URL = "http://localhost:3000/auth/google"  # TODO: переделать с учётом test и production
