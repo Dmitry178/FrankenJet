2b3676b0411c1a6fdc7a947908bcfa86cadb9744
Dmitry <dodegov@smarthomecrafters.ai>
2025-08-30 15:04:54 +0300

    Добавлена модель пользователей

---
+++ backend/app/db/models/users.py
@@ -0,0 +1,15 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.db.init_db import Base
+
+
+class Users(Base):
+
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    # при использовании CITEXT установить расширение CREATE EXTENSION IF NOT EXISTS citext;
+    email: Mapped[str] = mapped_column(String(254), unique=True, nullable=False)  # длина 254 символа согласно RFC 5321
+    hashed_password: Mapped[str] = mapped_column(String(64), nullable=False)
