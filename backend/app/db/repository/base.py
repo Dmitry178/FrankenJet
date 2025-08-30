9767733c1077c88779574cfc95469b9ddecc21db
Dmitry <dodegov@smarthomecrafters.ai>
2025-08-30 22:10:04 +0300

    Добавлены методы в базовый репозиторий

--- backend/app/db/repository/base.py
+++ backend/app/db/repository/base.py
@@ -1,3 +1,7 @@
+from pydantic import BaseModel
+from sqlalchemy import select, insert, exists
+
+
 class BaseRepository:
 
     model = None
@@ -6,3 +10,49 @@ class BaseRepository:
     def __init__(self, session):
         self.session = session
 
+    async def is_exists(self, *filters, **filter_by) -> bool:
+        """
+        Проверка на существование записи
+        """
+
+        query = select(
+            exists().select_from(self.model)
+            .where(*filters)
+        )
+        return (await self.session.execute(query)).scalar()
+
+    async def select_one_or_none(self, *filters, **filter_by):
+        """
+        Получение записи по фильтру
+        """
+
+        query = (
+            select(self.model.__table__.columns)
+            .filter(*filters)
+            .filter_by(**filter_by)
+        )
+        return (await self.session.execute(query)).mappings().one_or_none()
+
+    async def insert_data(self, **values):
+        """
+        Добавление данных
+        """
+
+        stmt = (
+            insert(self.model)
+            .values(**values)
+            .returning(self.model)
+        )
+        return (await self.session.execute(stmt)).mappings().one()
+
+    async def insert_model_data(self, data: BaseModel):
+        """
+        Добавление данных
+        """
+
+        stmt = (
+            insert(self.model)
+            .values(**data.model_dump())
+            .returning(self.model)
+        )
+        return (await self.session.execute(stmt)).mappings().one()
