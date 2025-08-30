94096aacede9f5557048d202f8abe8a3ed9601b4
Dmitry <dodegov@smarthomecrafters.ai>
2025-08-30 22:09:13 +0300

    Добавлены схемы регистрации, токенов, информации о пользователе

--- backend/app/schemas/auth.py
+++ backend/app/schemas/auth.py
@@ -1,6 +1,37 @@
 from pydantic import BaseModel, EmailStr
 
 
-class SUserLogin(BaseModel):
+class SLoginUser(BaseModel):
+    """
+    Схема логина пользователя
+    """
+
     email: EmailStr
     password: str
+
+
+class SRegisterUser(BaseModel):
+    """
+    Схема регистрации пользователя
+    """
+
+    email: EmailStr
+    hashed_password: str
+
+
+class SAuthTokens(BaseModel):
+    """
+    Схема jwt-токенов пользователя
+    """
+
+    access_token: str
+    refresh_token: str | None = None  # TODO: сделать обязательным после добавления refresh-токена
+
+
+class SUserInfo(BaseModel):
+    """
+    Схема информации о пользователе
+    """
+
+    id: int
+    email: EmailStr
