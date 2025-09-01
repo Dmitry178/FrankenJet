689fcac4d779c7d794858209c26ff0588352fe6d
Dmitry <dodegov@smarthomecrafters.ai>
2025-09-01 19:16:40 +0300

    Добавлены модели ролей и ассоциативная таблица связей пользователей и ролей

--- backend/app/db/models/users.py
+++ backend/app/db/models/users.py
@@ -1,10 +1,13 @@
-from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.db import Base
 
 
 class Users(Base):
+    """
+    Пользователи
+    """
 
     __tablename__ = "users"
 
@@ -18,5 +21,32 @@ class Users(Base):
     first_name: Mapped[str | None] = mapped_column(String(60))  # имя
     last_name: Mapped[str | None] = mapped_column(String(60))  # фамилия
 
+    roles: Mapped[list["Roles"]] = relationship(secondary="user_roles", back_populates="users")
+    # user_roles: Mapped[list["UserRoles"]] = relationship(back_populates="user")
 
-# TODO: добавить модели ролей пользователей
+
+class Roles(Base):
+    """
+    Роли пользователей
+    """
+
+    __tablename__ = "roles"
+
+    role: Mapped[str] = mapped_column(String(16), primary_key=True)
+
+    users: Mapped[list["Users"]] = relationship(secondary="user_roles", back_populates="roles")
+    # user_roles: Mapped[list["UserRoles"]] = relationship(back_populates="role")
+
+
+class UserRoles(Base):
+    """
+    Ассоциативная таблица для связи пользователей и ролей
+    """
+
+    __tablename__ = "user_roles"
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), primary_key=True)
+    role: Mapped[str] = mapped_column(ForeignKey("roles.role"), primary_key=True)
+
+    # user: Mapped["Users"] = relationship(back_populates="user_roles")
+    # role: Mapped["Roles"] = relationship(back_populates="user_roles")
