bcfd3f743846835ef9776079f58bb1d977dfe54c
Dmitry <dodegov@smarthomecrafters.ai>
2025-09-01 19:15:54 +0300

    Вынесен метод выпуска токенов, добавлен метод перевыпуска токенов

--- backend/app/services/auth.py
+++ backend/app/services/auth.py
@@ -1,54 +1,11 @@
-import jwt
-
-from datetime import datetime, timezone, timedelta
-from passlib.context import CryptContext
+from uuid import uuid4
 
 from app.core.config import settings
 from app.core.config_const import TOKEN_TYPE_ACCESS, TOKEN_TYPE_REFRESH
 from app.db.db_manager import DBManager
-from app.exceptions import UserNotFoundEx, PasswordIncorrectEx, UserExistsEx
+from app.exceptions import UserNotFoundEx, PasswordIncorrectEx, UserExistsEx, TokenTypeErrorEx, TokenInvalidEx
 from app.schemas.auth import SLoginUser, SRegisterUser, SAuthTokens, SUserInfo
-
-
-class AuthTokenService:
-
-    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-    def verify_password(self, plain_password, hashed_password):
-        return self.pwd_context.verify(plain_password, hashed_password)
-
-    def hash_password(self, password: str) -> str:
-        return self.pwd_context.hash(password)
-
-    @staticmethod
-    def create_jwt_token(payload_data: dict, expire: int) -> str:
-        """
-        Создание JWT-токена
-
-        :param payload_data: данные для упаковки в тело токена
-        :param expire: время жизни токена
-        """
-
-        iat = datetime.now(timezone.utc)
-        expire_date = iat + timedelta(minutes=expire)
-        payload = {**payload_data, "iat": iat, "exp": expire_date}
-        encoded_jwt = jwt.encode(payload=payload, key=settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
-
-        return encoded_jwt
-
-    @staticmethod
-    def decode_token(token: str) -> dict:
-        """
-        Извлечение данных из JWT токена
-        """
-
-        try:
-            payload = jwt.decode(jwt=token, key=settings.JWT_SECRET_KEY, algorithms=settings.JWT_ALGORITHM)
-            return payload
-
-        except:  # noqa
-            # TODO: сделать обработку исключений
-            return {}
+from app.services.security import SecurityService
 
 
 class AuthServices:
@@ -58,37 +15,71 @@ class AuthServices:
     def __init__(self, db: DBManager | None = None) -> None:
         self.db = db
 
+    @staticmethod
+    async def issue_tokens(user_id: int, email: str, roles: list | None) -> SAuthTokens:
+        """
+        Выпуск токенов
+        """
+
+        access_token = SecurityService().create_jwt_token(
+            {"id": user_id, "type": TOKEN_TYPE_ACCESS, "email": email, "roles": roles},
+            settings.JWT_ACCESS_EXPIRE_MINUTES
+        )
+
+        # TODO: сделать хранение токена (или jti) в базе
+        refresh_token = SecurityService().create_jwt_token(
+            {"id": user_id, "type": TOKEN_TYPE_REFRESH, "jti": str(uuid4())},
+            settings.JWT_REFRESH_EXPIRE_MINUTES
+        )
+
+        return SAuthTokens(access_token=access_token, refresh_token=refresh_token)
+
     async def login(self, data: SLoginUser) -> SAuthTokens:
         """
         Проверка пользователя и пароля, выпуск access и refresh токенов
         """
 
-        # TODO: добавить роли пользователя в базу и получение ролей пользователя
-        user = await self.db.users.select_one_or_none(email=data.email)
+        user = await self.db.users.get_user_with_roles(email=data.email)
         if not user:
             raise UserNotFoundEx
 
-        if not AuthTokenService().verify_password(data.password, user.hashed_password):
+        roles = [role.role for role in user.roles]  # список ролей пользователя
+
+        if not SecurityService().verify_password(data.password, user.hashed_password):
             raise PasswordIncorrectEx
 
-        # TODO: добавить роли в access_token
-        access_token = AuthTokenService().create_jwt_token(
-            {"id": user.id, "type": TOKEN_TYPE_ACCESS, "email": data.email},
-            settings.JWT_ACCESS_EXPIRE_MINUTES
-        )
-        refresh_token = AuthTokenService().create_jwt_token(
-            {"id": user.id, "type": TOKEN_TYPE_REFRESH},
-            settings.JWT_REFRESH_EXPIRE_MINUTES
-        )
+        return await self.issue_tokens(user.id, user.email, roles)
 
-        return SAuthTokens(access_token=access_token, refresh_token=refresh_token)
+    async def refresh(self, refresh_token: str) -> SAuthTokens:
+        """
+        Перевыпуск access и refresh токенов
+        """
+
+        # TODO: сделать отзыв токена в базе
+
+        refresh_token_payload = SecurityService().decode_token(refresh_token)
+        if not refresh_token_payload:
+            raise TokenInvalidEx
+
+        if refresh_token_payload.get("type") != TOKEN_TYPE_REFRESH:
+            raise TokenTypeErrorEx
+
+        user_id = refresh_token_payload.get("id")
+
+        user = await self.db.users.get_user_with_roles(id=user_id)
+        if not user:
+            raise UserNotFoundEx
+
+        roles = [role.role for role in user.roles]
+
+        return await self.issue_tokens(user_id, user.email, roles)
 
     async def register_user(self, data: SLoginUser) -> None:
         """
         Регистрация пользователя
         """
 
-        hashed_password = AuthTokenService().hash_password(data.password)
+        hashed_password = SecurityService().hash_password(data.password)
         new_user_data = SRegisterUser(email=data.email, hashed_password=hashed_password)
 
         # TODO: добавить отправку и валидацию кода подтверждения на email
