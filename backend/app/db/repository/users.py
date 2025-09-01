3094169ade3b5152400b81af4121c82754c006fd
Dmitry <dodegov@smarthomecrafters.ai>
2025-09-01 19:16:58 +0300

    Добавлено получение пользователя с его ролями

--- backend/app/db/repository/users.py
+++ backend/app/db/repository/users.py
@@ -1,3 +1,6 @@
+from sqlalchemy import select
+from sqlalchemy.orm import selectinload
+
 from app.db.models.users import Users
 from app.db.repository.base import BaseRepository
 
@@ -9,3 +12,17 @@ class UsersRepository(BaseRepository):
 
     model = Users
     # mapper = UsersDataMapper  # TODO: добавить mapper
+
+    async def get_user_with_roles(self, *filters, **filter_by):
+        """
+        Получение пользователя с его ролями
+        """
+
+        query = (
+            select(Users)
+            .filter(*filters)
+            .filter_by(**filter_by)
+            .options(selectinload(Users.roles))
+        )
+
+        return (await self.session.execute(query)).scalars().one_or_none()
