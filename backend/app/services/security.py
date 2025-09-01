ebe8a2e212e33afd943a381f847f29c3d4b655d8
Dmitry <dodegov@smarthomecrafters.ai>
2025-09-01 19:17:21 +0300

    SecurityService вынесен в отдельный сервис

---
+++ backend/app/services/security.py
@@ -0,0 +1,47 @@
+import jwt
+
+from datetime import datetime, timezone, timedelta
+from passlib.context import CryptContext
+
+from app.core.config import settings
+
+
+class SecurityService:
+
+    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+    def verify_password(self, plain_password, hashed_password):
+        return self.pwd_context.verify(plain_password, hashed_password)
+
+    def hash_password(self, password: str) -> str:
+        return self.pwd_context.hash(password)
+
+    @staticmethod
+    def create_jwt_token(payload_data: dict, expire: int) -> str:
+        """
+        Создание JWT-токена
+
+        :param payload_data: данные для упаковки в тело токена
+        :param expire: время жизни токена
+        """
+
+        iat = datetime.now(timezone.utc)
+        expire_date = iat + timedelta(minutes=expire)
+        payload = {**payload_data, "iat": iat, "exp": expire_date}
+        encoded_jwt = jwt.encode(payload=payload, key=settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
+
+        return encoded_jwt
+
+    @staticmethod
+    def decode_token(token: str) -> dict:
+        """
+        Извлечение данных из JWT токена
+        """
+
+        try:
+            payload = jwt.decode(jwt=token, key=settings.JWT_SECRET_KEY, algorithms=settings.JWT_ALGORITHM)
+            return payload
+
+        except:  # noqa
+            # TODO: сделать обработку исключений
+            return {}
