ff9e15a2e517e1beb7ccdbfc75a77c3d0ce7a546
Dmitry <dodegov@smarthomecrafters.ai>
2025-09-01 13:09:40 +0300

    Переделана зависимость получения id пользователя из кук на заголовки

--- backend/app/dependencies/auth.py
+++ backend/app/dependencies/auth.py
@@ -1,27 +1,33 @@
-from fastapi import Depends, HTTPException, Request
+from fastapi import Depends
 from typing import Annotated
 
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from jwt import ExpiredSignatureError, InvalidTokenError
+
+from app.core.config_const import TOKEN_TYPE_ACCESS
+from app.exceptions import TokenTypeErrorEx, AuthUserErrorEx
 from app.services.auth import AuthTokenService
 
+security = HTTPBearer()
 
-def get_auth_user_id(request: Request) -> int:
+
+async def get_auth_user_id(credentials: HTTPAuthorizationCredentials = Depends(security)) -> int:
     """
-    Получение id текущего аутентифицированного пользователя из cookies
+    Получение id текущего аутентифицированного пользователя из заголовков
     """
 
-    access_token = request.cookies.get("access_token", None)
-    if not access_token:
-        raise HTTPException(status_code=401, detail="Token not found")
+    try:
+        # получение тела токена
+        token_payload = AuthTokenService.decode_token(credentials.credentials)
 
-    decoded_token = AuthTokenService().decode_token(access_token)
-    if not decoded_token:
-        raise HTTPException(status_code=401, detail="Token error")
+        # проверка типа токена
+        if token_payload.get("type") != TOKEN_TYPE_ACCESS:
+            raise TokenTypeErrorEx
 
-    user_id = decoded_token.get("id")
-    if not user_id:
-        raise HTTPException(status_code=401, detail="User not found")
+        return token_payload.get("id")
 
-    return user_id
+    except (ExpiredSignatureError, InvalidTokenError, TokenTypeErrorEx, Exception) as ex:
+        raise AuthUserErrorEx from ex
 
 
 UserIdDep = Annotated[int, Depends(get_auth_user_id)]
