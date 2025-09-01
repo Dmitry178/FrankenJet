82142c41f188b424d6c4bf72cb8f6cec6e4a55a4
Dmitry <dodegov@smarthomecrafters.ai>
2025-09-01 13:07:43 +0300

    Добавлены константы типов токенов

--- backend/app/core/config_const.py
+++ backend/app/core/config_const.py
@@ -6,3 +6,7 @@ OAUTH2_VK_URL = "https://id.vk.com/authorize"
 OAUTH2_VK_TOKEN_URL = "https://api.vk.com/method/auth.exchangeSilentAuthToken"
 OAUTH2_VK_REDIRECT_URL = "http://localhost/auth/vk"  # TODO: переделать с учётом test и production
 # VK при использовании http поддерживает только localhost (без порта)  # TODO учесть этот момент
+
+# типы токенов
+TOKEN_TYPE_ACCESS = "access"
+TOKEN_TYPE_REFRESH = "refresh"
