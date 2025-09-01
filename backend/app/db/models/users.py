d4370acf201a4a5c878eb0da9aeaa11c1585ac06
Dmitry <dodegov@smarthomecrafters.ai>
2025-09-01 13:08:40 +0300

    Добавлены поля в модель Users

--- backend/app/db/models/users.py
+++ backend/app/db/models/users.py
@@ -8,8 +8,15 @@ class Users(Base):
 
     __tablename__ = "users"
 
+    # при использовании UUID установить расширение "uuid-ossp"
     id: Mapped[int] = mapped_column(primary_key=True)
 
-    # при использовании CITEXT установить расширение CREATE EXTENSION IF NOT EXISTS citext;
+    # при использовании CITEXT установить расширение "citext";
     email: Mapped[str] = mapped_column(String(254), unique=True, nullable=False)  # длина 254 символа согласно RFC 5321
-    hashed_password: Mapped[str] = mapped_column(String(64), nullable=False)
+    hashed_password: Mapped[str | None] = mapped_column(String(64), nullable=True)  # для OAuth2 поле пустое (см. доку)
+    full_name: Mapped[str | None] = mapped_column(String(60))  # полное имя
+    first_name: Mapped[str | None] = mapped_column(String(60))  # имя
+    last_name: Mapped[str | None] = mapped_column(String(60))  # фамилия
+
+
+# TODO: добавить модели ролей пользователей
