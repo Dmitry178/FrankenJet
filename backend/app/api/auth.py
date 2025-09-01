5c531d03a27f32f7791410f4ea68e72893ed47f0
Dmitry <dodegov@smarthomecrafters.ai>
2025-09-01 13:05:33 +0300

    Убран /logout (перенесено на сторону frontend)

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -11,41 +11,17 @@ auth_router = APIRouter(prefix="/auth", tags=["Auth"])
 
 
 @auth_router.post("/login", summary="Аутентификация пользователя по email и паролю")
-async def user_login(response: Response, data: SLoginUser, db: DDB):
+async def user_login(data: SLoginUser, db: DDB):
     """
     Аутентификация пользователя
     """
 
     try:
         tokens = await AuthServices(db).login(data)
-        response.set_cookie(key="access", value=tokens.access_token)
-        response.set_cookie(key="refresh", value=tokens.refresh_token)
-
-        return {**status_ok, "data": tokens.model_dump(exclude_none=True)}
+        return {**status_ok, "data": tokens.model_dump()}
 
     except (UserNotFoundEx, PasswordIncorrectEx) as ex:
         raise HTTPException(status_code=ex.status_code, detail=ex.detail)
-
-
-@auth_router.post("/logout", summary="Выход из аккаунта")
-async def user_logout(response: Response):
-    """
-    Выход пользователя
-    """
-
-    try:
-        response.delete_cookie(
-            key="access_token",
-            # secure=True,
-            # httponly=True,
-            # samesite="lax",
-        )
-        return status_ok
-
-    except:  # noqa  # TODO: доработать
-        pass
-
-    return {"status": "error", "detail": "Ошибка удаления cookie"}  # TODO: доработать
 
 
 @auth_router.post("/register", summary="Регистрация пользователя")
@@ -71,4 +47,4 @@ async def get_user_info(user_id: UserIdDep, db: DDB):
     # TODO: обернуть в try/except
     user_info = await AuthServices(db).get_user_info(user_id)
 
-    return {**status_ok, "data": user_info.model_dump(exclude_none=True)}
+    return {**status_ok, "data": user_info.model_dump()}
