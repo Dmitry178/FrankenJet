540b288f9bfed48bef0fef336581fc175edd21d8
Dmitry <dodegov@smarthomecrafters.ai>
2025-09-02 21:59:52 +0300

    Добавлены модели

--- backend/app/db/models/__init__.py
+++ backend/app/db/models/__init__.py
@@ -1,7 +1,17 @@
 # ruff: noqa
 
-from app.db.models.users import Users
+from app.db.models.auth import (
+    RefreshTokens,
+)
+from app.db.models.users import (
+    Roles,
+    UserRoles,
+    Users,
+)
 
 __all__ = [
+    "RefreshTokens",
+    "Roles",
+    "UserRoles",
     "Users",
 ]
