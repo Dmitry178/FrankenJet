1277cbfdc0a9731a67d139555f17b9296c765590
Dmitry <dodegov@smarthomecrafters.ai>
2025-09-04 23:08:21 +0300

    Доработан auth.js

--- frontend/src/stores/auth.js
+++ frontend/src/stores/auth.js
@@ -2,14 +2,20 @@ import { defineStore } from 'pinia';
 
 export const useAuthStore = defineStore('auth', {
   state: () => ({
-    accessToken: null
+    accessToken: localStorage.getItem('accessToken') || null
   }),
   actions: {
     setAccessToken(token) {
       this.accessToken = token;
+      if (token) {
+          localStorage.setItem('accessToken', token);
+      } else {
+          localStorage.removeItem('accessToken');
+      }
     },
     clearAccessToken() {
       this.accessToken = null;
+      localStorage.removeItem('accessToken');
     }
-  }
+  },
 });
