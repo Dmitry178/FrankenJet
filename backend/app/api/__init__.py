037c6d8119e2f7fd8ccef0a6c1e024e3bcc72348
Dmitry <dodegov@smarthomecrafters.ai>
2025-09-05 19:58:12 +0300

    Добавлен роутер app_router

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
+from app.api.app import app_router
 from app.api.auth import auth_router
 from app.api.oauth2 import oauth_router
 
 router = APIRouter()
+router.include_router(app_router)
 router.include_router(auth_router)
 router.include_router(oauth_router)
