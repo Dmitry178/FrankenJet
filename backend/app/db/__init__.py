7d8565605829de183592ce475208aea8ada7a8cf
Dmitry <dodegov@smarthomecrafters.ai>
2025-09-06 13:32:27 +0300

    Доработан класс Base

--- backend/app/db/__init__.py
+++ backend/app/db/__init__.py
@@ -2,7 +2,8 @@ from sqlalchemy import NullPool
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 
-from app.core.config import settings
+from app.core.config_env import settings
+from app.db.types import project_metadata, annotation_map
 
 engine = create_async_engine(settings.db_url, echo=False)
 engine_null_pool = create_async_engine(settings.db_url, poolclass=NullPool)
@@ -12,4 +13,5 @@ async_session_maker_null_pool = async_sessionmaker(bind=engine_null_pool, expire
 
 
 class Base(DeclarativeBase):
-    pass
+    metadata = project_metadata
+    type_annotation_map = annotation_map
