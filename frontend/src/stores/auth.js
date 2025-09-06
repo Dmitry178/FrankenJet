89f48295defb80b2288ca06aca63e55e9cd0c36c
Dmitry <dodegov@smarthomecrafters.ai>
2025-09-06 13:33:16 +0300

    Добавлены данные пользователя в хранилище

--- frontend/src/stores/auth.js
+++ frontend/src/stores/auth.js
@@ -2,20 +2,49 @@ import { defineStore } from 'pinia';
 
 export const useAuthStore = defineStore('auth', {
   state: () => ({
-    accessToken: localStorage.getItem('accessToken') || null
+    accessToken: null,
+    user: {
+      email: null,
+      fullName: null,
+      firstName: null,
+      lastName: null,
+      picture: null,
+    },
+    roles: [],
   }),
   actions: {
     setAccessToken(token) {
       this.accessToken = token;
-      if (token) {
-          localStorage.setItem('accessToken', token);
-      } else {
-          localStorage.removeItem('accessToken');
-      }
     },
     clearAccessToken() {
       this.accessToken = null;
-      localStorage.removeItem('accessToken');
+      this.user = {
+        email: null,
+        fullName: null,
+        firstName: null,
+        lastName: null,
+        picture: null,
+      };
+      this.roles = [];
+    },
+    setUser(userData) {
+      this.user = { ...userData }; // разворачиваем userData, чтобы избежать реактивности исходного объекта
+    },
+    clearUser() {
+      this.user = {
+        email: null,
+        fullName: null,
+        firstName: null,
+        lastName: null,
+        picture: null,
+      };
+      this.roles = [];
+    },
+    setRoles(roles) {
+      this.roles = roles;
+    },
+    clearRoles() {
+      this.roles = [];
     }
   },
 });
