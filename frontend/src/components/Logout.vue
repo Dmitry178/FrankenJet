24c37110745cdc34ab98b79ff4c2034b68fb2d2c
Dmitry <dodegov@smarthomecrafters.ai>
2025-09-07 17:59:18 +0300

    Сделан вывод фотки профиля, если она есть

--- frontend/src/components/Logout.vue
+++ frontend/src/components/Logout.vue
@@ -2,15 +2,24 @@
   <v-menu offset="0" :close-on-content-click="false">
     <template v-slot:activator="{ props }">
       <v-btn class="ml-2" v-bind="props" icon>
-        <v-icon left>mdi-account-circle</v-icon>
+        <v-img
+          v-if="authStore.user.picture"
+          :src="authStore.user.picture"
+          alt=""
+          width="32"
+          height="32"
+          class="rounded-circle"
+        />
+        <v-icon v-else left>mdi-account-circle</v-icon>
       </v-btn>
     </template>
-    <v-card width="200">
-      <v-card-title class="text-center">Профиль</v-card-title>
+    <v-card width="250">
+      <v-card-title class="text-center">{{ authStore.user.firstName }}</v-card-title>
       <v-card-actions class="d-flex flex-column">
         <router-link to="/profile" class="v-btn v-btn--text">
-          Редактировать профиль
+          Профиль
         </router-link>
+        <v-divider></v-divider>
         <v-btn color="error" block @click="logout">Выйти</v-btn>
       </v-card-actions>
     </v-card>
@@ -33,7 +42,10 @@ export default {
       await router.push('/');
     };
 
-    return { logout };
+    return {
+      logout,
+      authStore,
+    };
   }
 };
 </script>
