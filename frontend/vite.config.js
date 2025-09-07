c3e7eab2a7c6271ebc0ebd424c6c1f6ac319b6a2
Dmitry <dodegov@smarthomecrafters.ai>
2025-09-07 18:00:25 +0300

    Подключен vite-plugin-vuetify

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,27 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
+import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      vue(),
+      vuetify({ autoImport: true }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)) // Добавьте это
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  optimizeDeps: {
+    include: [
+      'vuetify',
+      'vuetify/components',
+      'vuetify/directives',
+    ],
+  },
+  build: {
+    transpileDependencies: ['vuetify'],
+  },
 })
