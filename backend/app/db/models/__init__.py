cb4af537c23938946704b05ce4931c2ddc7ccead
Dmitry <dodegov@smarthomecrafters.ai>
2025-09-07 16:05:17 +0300

    Добавлены модели

--- backend/app/db/models/__init__.py
+++ backend/app/db/models/__init__.py
@@ -1,17 +1,35 @@
 # ruff: noqa
 
+from app.db.models.articles import (
+    Aircraft,
+    AircraftDesignersAssociation,
+    AircraftManufacturersAssociation,
+    Countries,
+    DesignBureaus,
+    Designers,
+    DesignersBureausAssociation,
+    Manufacturers,
+)
 from app.db.models.auth import (
     RefreshTokens,
 )
 from app.db.models.users import (
     Roles,
-    UserRoles,
     Users,
+    UsersRolesAssociation,
 )
 
 __all__ = [
+    "Aircraft",
+    "AircraftDesignersAssociation",
+    "AircraftManufacturersAssociation",
+    "Countries",
+    "DesignBureaus",
+    "Designers",
+    "DesignersBureausAssociation",
+    "Manufacturers",
     "RefreshTokens",
     "Roles",
-    "UserRoles",
     "Users",
+    "UsersRolesAssociation",
 ]
