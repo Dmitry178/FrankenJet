a544b5077e4d864d9ea5dea0ce279b3ac5ca2e21
Dmitry <dodegov@smarthomecrafters.ai>
2025-09-08 23:16:57 +0300

    Добавлены роутеры aircraft_router и articles_router

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
+from app.api.aircraft import aircraft_router
 from app.api.app import app_router
+from app.api.articles import articles_router
 from app.api.auth import auth_router
 from app.api.oauth2 import oauth_router
 
@@ -8,3 +10,5 @@ router = APIRouter()
 router.include_router(app_router)
 router.include_router(auth_router)
 router.include_router(oauth_router)
+router.include_router(aircraft_router)
+router.include_router(articles_router)
