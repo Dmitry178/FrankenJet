202c2a61cf2aebde4b27b78acd406e5c5eb17dc5
Dmitry <dodegov@smarthomecrafters.ai>
2025-09-09 23:00:48 +0300

    Добавлен сервисный слой конструкторов

---
+++ backend/app/services/designers.py
@@ -0,0 +1,46 @@
+from uuid import UUID
+
+from app.db.db_manager import DBManager
+from app.schemas.articles import SCountriesFilters, SDesigners
+
+
+class DesignersServices:
+
+    db: DBManager | None
+
+    def __init__(self, db: DBManager | None = None) -> None:
+        self.db = db
+
+    async def get_designers(self, filters: SCountriesFilters | None = None):
+        """
+        Получение списка конструкторов
+        """
+
+        filter_by = filters.model_dump(exclude_none=True) if filters else {}
+        return await self.db.articles.designers.select_all(filter_by)
+
+    async def add_designer(self, data: SDesigners):
+        """
+        Добавление карточки конструктора
+        """
+
+        return await self.db.articles.countries.insert_one(data)
+
+    async def edit_designer(self, designer_id: UUID, data: SDesigners, exclude_unset=False):
+        """
+        Редактирование карточки конструктора
+        """
+
+        return await self.db.articles.designers.update_one(
+            data,
+            id=designer_id,
+            exclude_unset=exclude_unset,
+            commit=True,
+        )
+
+    async def delete_designer(self, designer_id: UUID):
+        """
+        Удаление карточки конструктора
+        """
+
+        return await self.db.articles.designers.delete_one(id=designer_id)
