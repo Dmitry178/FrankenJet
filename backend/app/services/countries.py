4323906f73aa66cb27f34ca9adfccd7614a209aa
Dmitry <dodegov@smarthomecrafters.ai>
2025-09-09 23:00:41 +0300

    Добавлен сервисный слой стран

---
+++ backend/app/services/countries.py
@@ -0,0 +1,45 @@
+from uuid import UUID
+
+from app.db.db_manager import DBManager
+from app.schemas.articles import SCountries
+
+
+class CountriesServices:
+
+    db: DBManager | None
+
+    def __init__(self, db: DBManager | None = None) -> None:
+        self.db = db
+
+    async def get_countries(self):
+        """
+        Получение списка стран
+        """
+
+        return await self.db.articles.countries.select_all()
+
+    async def add_country(self, data: SCountries):
+        """
+        Добавление карточки страны
+        """
+
+        return await self.db.articles.countries.insert_one(data)
+
+    async def edit_country(self, aircraft_id: UUID, data: SCountries, exclude_unset=False):
+        """
+        Редактирование карточки страны
+        """
+
+        return await self.db.articles.countries.update_one(
+            data,
+            id=aircraft_id,
+            exclude_unset=exclude_unset,
+            commit=True,
+        )
+
+    async def delete_country(self, aircraft_id: UUID):
+        """
+        Удаление карточки страны
+        """
+
+        return await self.db.articles.countries.delete_one(id=aircraft_id)
