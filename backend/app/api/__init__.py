989530e58184a2faef5b6a60003f4e282e1c1fc4
Dmitry <dodegov@smarthomecrafters.ai>
2025-09-09 22:59:09 +0300

    Добавлены роутеры стран, конструкторских бюро, конструкторов, производителей

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -2,8 +2,11 @@ from fastapi import APIRouter
 
 from app.api.aircraft import aircraft_router
 from app.api.app import app_router
-from app.api.articles import articles_router
+from app.api.bureaus import bureaus_router
+from app.api.countries import countries_router
 from app.api.auth import auth_router
+from app.api.designers import designers_router
+from app.api.manufacturers import manufacturers_router
 from app.api.oauth2 import oauth_router
 
 router = APIRouter()
@@ -11,4 +14,7 @@ router.include_router(app_router)
 router.include_router(auth_router)
 router.include_router(oauth_router)
 router.include_router(aircraft_router)
-router.include_router(articles_router)
+router.include_router(countries_router)
+router.include_router(bureaus_router)
+router.include_router(designers_router)
+router.include_router(manufacturers_router)
