5a441d0384e0dd5ba6200c24af3cbb0aa245b44d
Dmitry <dodegov@smarthomecrafters.ai>
2025-09-18 21:03:41 +0300

    Добавлена репозиторий статей

--- backend/app/db/repository/articles.py
+++ backend/app/db/repository/articles.py
@@ -1,43 +1,10 @@
-from app.db.models import Aircraft, Countries, Designers
-from app.db.models.articles import Manufacturers, DesignBureaus
+from app.db.models import Articles
 from app.db.repository.base import BaseRepository
 
 
-class CountriesRepository(BaseRepository):
+class ArticlesRepository(BaseRepository):
     """
-    Репозиторий модели стран
-    """
-
-    model = Countries
-
-
-class AircraftRepository(BaseRepository):
-    """
-    Репозиторий модели воздушных судов
+    Репозиторий модели статей
     """
 
-    model = Aircraft
-
-
-class DesignersRepository(BaseRepository):
-    """
-    Репозиторий модели конструкторов
-    """
-
-    model = Designers
-
-
-class ManufacturersRepository(BaseRepository):
-    """
-    Репозиторий модели производителей
-    """
-
-    model = Manufacturers
-
-
-class DesignBureausRepository(BaseRepository):
-    """
-    Репозиторий модели конструкторских бюро
-    """
-
-    model = DesignBureaus
+    model = Articles
