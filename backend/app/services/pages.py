3146264a0506f530c70108134791c7b725541028
Dmitry <dodegov@smarthomecrafters.ai>
2025-09-22 19:22:28 +0300

    Добавлен сервисный слой получения информации для страниц на frontend

---
+++ backend/app/services/pages.py
@@ -0,0 +1,27 @@
+from app.db.db_manager import DBManager
+
+
+class PagesService:
+
+    db: DBManager | None
+
+    def __init__(self, db: DBManager | None = None) -> None:
+        self.db = db
+
+    async def home(self) -> dict:
+        """
+        Информация для главной страницы
+        """
+
+        context = {}
+
+        # случайные статьи
+        columns = ["slug", "title", "summary", "thumbnail_url"]
+        random_articles = await self.db.articles.select_random(columns=columns, limit=4, is_published=True) or []
+        context["articles"] = random_articles
+
+        # случайные факты
+        random_facts = await self.db.facts.select_random(limit=5) or []
+        context["facts"] = [item["fact"] for item in random_facts]
+
+        return context
