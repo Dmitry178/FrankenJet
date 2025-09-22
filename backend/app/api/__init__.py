ceff5df792bd2d4d26af7d23a80d7a6e75794b12
Dmitry <dodegov@smarthomecrafters.ai>
2025-09-22 19:11:07 +0300

    Добавлен роутер информации для страниц фронтенда

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -9,11 +9,13 @@ from app.api.countries import countries_router
 from app.api.designers import designers_router
 from app.api.manufacturers import manufacturers_router
 from app.api.oauth2 import oauth_router
+from app.api.pages import pages_router
 
 router = APIRouter()
 router.include_router(app_router)
 router.include_router(auth_router)
 router.include_router(oauth_router)
+router.include_router(pages_router)
 router.include_router(articles_router)
 router.include_router(aircraft_router)
 router.include_router(countries_router)
