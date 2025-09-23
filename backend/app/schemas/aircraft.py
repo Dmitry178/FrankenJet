12fd7ceb386cfb8b19904decc98903bc1e86ca7d
Dmitry <dodegov@smarthomecrafters.ai>
2025-09-23 20:34:48 +0300

    Доработаны схемы

--- backend/app/schemas/aircraft.py
+++ backend/app/schemas/aircraft.py
@@ -10,9 +10,9 @@ class SAircraftFilters(BaseModel):
     Схема фильтров воздушных судов
     """
 
-    country: str | None = Field(None, description="Страна"),
-    engine_type: EngineTypes | None = Field(None, description="Тип двигателя"),
-    status: AircraftStatus | None = Field(None, description="Статусу воздушного судна"),
+    country: str | None = Field(None, description="Страна")
+    engine_type: EngineTypes | None = Field(None, description="Тип двигателя")
+    status: AircraftStatus | None = Field(None, description="Статусу воздушного судна")
 
 
 class SAircraft(BaseModel):
@@ -22,29 +22,29 @@ class SAircraft(BaseModel):
 
     slug: str = Field(..., max_length=64, description="Строковый идентификатор")
     name: str = Field(..., max_length=32, description="Название")
-    manufacturer_id: UUID = Field(None, description="ID производителя")
+    manufacturer_id: UUID | None = Field(None, description="ID производителя")
     country_id: str = Field(..., min_length=2, max_length=2, description="Двухбуквенный ISO-код страны")
-    aircraft_type: str = Field(None, max_length=32, description="Тип воздушного судна")  # TODO: уточнить длину (enum)
-    first_flight: date = Field(None, description="Первый полёт")
-    wingspan: float = Field(None, description="Размах крыльев в метрах")
-    length: float = Field(None, description="Длина воздушного судна в метрах")
-    height: float = Field(None, description="Высота воздушного судна в метрах")
-    max_takeoff_weight: float = Field(None, description="Максимальный взлетный вес в килограммах")
-    engine_type: str = Field(None, max_length=32, description="Тип двигателя")  # TODO: уточнить длину (enum)
-    number_of_engines: int = Field(None, description="Количество двигателей")
-    max_speed: int = Field(None, description="Максимальная скорость в км/ч")
-    cruise_speed: int = Field(None, description="Крейсерская скорость в км/ч")
-    range: int = Field(None, description="Дальность полета в км")
-    service_ceiling: int = Field(None, description="Практический потолок в метрах")
-    crew: int = Field(None, description="Экипаж (количество человек)")
-    # capacity: int = Field(None, description="Вместимость (количество пассажиров или полезной нагрузки)")
-    icao_designator: str = Field(None, max_length=4, description="Четырёхбуквенный код ИКАО")
-    iata_designator: str = Field(None, max_length=3, description="Двух-трёхбуквенный код ИАТА")
-    status: str = Field(None, max_length=32, description="Статус самолета")  # TODO: уточнить длину (enum)
-    # variants: str = Field(None, description="Описание модификаций и вариантов самолета")
-    year_of_manufacture: int = Field(None, description="Дата начала производства")
-    first_used: date = Field(None, description="Дата начала эксплуатации")
-    last_used: date = Field(None, description="Дата окончания эксплуатации")
+    aircraft_type: str | None = Field(None, max_length=32, description="Тип воздушного судна")  # TODO: уточнить размер
+    first_flight: date | None = Field(None, description="Первый полёт")
+    wingspan: float | None = Field(None, description="Размах крыльев в метрах")
+    length: float | None = Field(None, description="Длина воздушного судна в метрах")
+    height: float | None = Field(None, description="Высота воздушного судна в метрах")
+    max_takeoff_weight: float | None = Field(None, description="Максимальный взлетный вес в килограммах")
+    engine_type: str | None = Field(None, max_length=32, description="Тип двигателя")  # TODO: уточнить длину (enum)
+    number_of_engines: int | None = Field(None, description="Количество двигателей")
+    max_speed: int | None = Field(None, description="Максимальная скорость в км/ч")
+    cruise_speed: int | None = Field(None, description="Крейсерская скорость в км/ч")
+    range: int | None = Field(None, description="Дальность полета в км")
+    service_ceiling: int | None = Field(None, description="Практический потолок в метрах")
+    crew: int | None = Field(None, description="Экипаж (количество человек)")
+    # capacity: int | None = Field(None, description="Вместимость (количество пассажиров или полезной нагрузки)")
+    icao_designator: str | None = Field(None, max_length=4, description="Четырёхбуквенный код ИКАО")
+    iata_designator: str | None = Field(None, max_length=3, description="Двух-трёхбуквенный код ИАТА")
+    status: str | None = Field(None, max_length=32, description="Статус самолета")  # TODO: уточнить длину (enum)
+    # variants: str | None = Field(None, description="Описание модификаций и вариантов самолета")
+    year_of_manufacture: int | None = Field(None, description="Дата начала производства")
+    first_used: date | None = Field(None, description="Дата начала эксплуатации")
+    last_used: date | None = Field(None, description="Дата окончания эксплуатации")
 
 
 class SCountriesFilters(BaseModel):
@@ -52,7 +52,7 @@ class SCountriesFilters(BaseModel):
     Схема фильтра по странам
     """
 
-    country: str | None = Field(None, description="Страна"),
+    country: str | None = Field(None, description="Страна")
 
 
 class SCountries(BaseModel):
@@ -61,9 +61,9 @@ class SCountries(BaseModel):
     """
 
     id: str = Field(..., min_length=2, max_length=2, description="Двухбуквенный ISO-код страны")
-    name: str = Field(None, max_length=32, description="Название страны")
+    name: str = Field(..., max_length=32, description="Название страны")
     iso_code: str | None = Field(None, max_length=3, description="Трёхбуквенный ISO-код страны")
-    flag_image_url: str = Field(None, max_length=128, description="URL изображения флага страны")
+    flag_image_url: str | None = Field(None, max_length=128, description="URL изображения флага страны")
 
 
 class SDesigners(BaseModel):
@@ -74,11 +74,11 @@ class SDesigners(BaseModel):
     country_id: str = Field(..., min_length=2, max_length=2, description="Двухбуквенный ISO-код страны")
     slug: str = Field(..., max_length=64, description="Строковый идентификатор")
     name: str = Field(..., max_length=32, description="Имя конструктора")
-    birth_date: date = Field(None)
-    death_date: date = Field(None)
-    known_for: str = Field(None, description="Чем знаменит конструктор")
+    birth_date: date | None = Field(None)
+    death_date: date | None = Field(None)
+    known_for: str | None = Field(None, description="Чем знаменит конструктор")
     biography: str = Field(..., description="Биография")
-    image_url: str = Field(None, description="URL основного изображения")
+    image_url: str | None = Field(None, description="URL основного изображения")
 
 
 class SDesignBureaus(BaseModel):
--- backend/app/schemas/articles.py
+++ backend/app/schemas/articles.py
@@ -7,18 +7,18 @@ class SArticles(BaseModel):
     Схема статей
     """
 
-    article_category: str = Field(None, max_length=32, description="Категория статьи")  # TODO: уточнить длину (enum)
+    article_category: str | None = Field(None, max_length=32, description="Категория статьи")  # TODO: уточнить размер
     slug: str = Field(..., max_length=64, description="Строковый идентификатор")
     title: str = Field(..., max_length=32, description="Заголовок статьи")
-    summary: str = Field(None, description="Краткое описание статьи")
+    summary: str | None = Field(None, description="Краткое описание статьи")
     content: str = Field(..., description="Текст статьи")
-    image_url: str = Field(None, max_length=128, description="Главное изображение")
-    thumbnail_url: str = Field(None, max_length=128, description="Миниатюра")
-    meta_title: str = Field(None, max_length=64, description="Мета-информация для SEO (название)")
-    meta_description: str = Field(None, max_length=128, description="Мета-информация для SEO (описание)")
+    image_url: str | None = Field(None, max_length=128, description="Главное изображение")
+    thumbnail_url: str | None = Field(None, max_length=128, description="Миниатюра")
+    meta_title: str | None = Field(None, max_length=64, description="Мета-информация для SEO (название)")
+    meta_description: str | None = Field(None, max_length=128, description="Мета-информация для SEO (описание)")
     view_count: int = Field(..., ge=0, description="Количество просмотров страницы")
-    seo_keywords: str = Field(None, max_length=128, description="Ключевые слова SEO")
+    seo_keywords: str | None = Field(None, max_length=128, description="Ключевые слова SEO")
     is_published: bool = Field(False, description="Статья опубликована")
     is_archived: bool = Field(False, description="Статья в архиве")
-    published_at: datetime = Field(None, description="Дата публикации")
-    archived_at: datetime = Field(None, description="Дата архива")
+    published_at: datetime | None = Field(None, description="Дата публикации")
+    archived_at: datetime | None = Field(None, description="Дата архива")
