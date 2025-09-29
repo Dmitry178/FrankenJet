6f9781d8c49eefdfed38d3c29de0441ad6c66981
Dmitry <dodegov@smarthomecrafters.ai>
2025-09-29 22:10:51 +0300

    Добавлены схемы бота

---
+++ backend/app/schemas/bot.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel, Field, EmailStr
+
+
+class SBotNotification(BaseModel):
+    """
+    Схема для отправки уведомления в телеграм-бот
+    """
+
+    notification: str  # уведомление
+
+
+class SBotAuthNotification(BaseModel):
+    """
+    Схема для отправки уведомления об аутентификации администратора в телеграм-бот
+    """
+
+    user_id: int = Field(..., alias="user-id")  # id пользователя
+    user_name: str | None = Field(None, alias="user-name")  # имя пользователя
+    email: str | EmailStr = Field(..., alias="email")  # email пользователя
+    roles: list | None = Field(None, alias="roles")  # роли пользователя
+    client_ip: str | None = Field(None, alias="client-ip")  # ip-адрес
+    user_agent: str | None = Field(None, alias="user-agent")  # данные браузера
+
+    class Config:
+        populate_by_name = True
+
+
+class SBotModeration(BaseModel):
+    """
+    Схема для отправки комментария на модерацию в телеграм-бот
+    """
+
+    id: int  # id комментария
+    comment: str  # текст комментария
