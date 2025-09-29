eb69473abef46adcd475c9f250759d1f6aa6951e
Dmitry <dodegov@smarthomecrafters.ai>
2025-09-29 22:02:37 +0300

    Перенесена инициализация s3-менеджера

--- backend/app/core/__init__.py
+++
@@ -1,8 +0,0 @@
-from app.core.config_env import settings
-from app.core.s3_manager import S3Manager
-
-s3_manager = S3Manager(
-    access_key_id=settings.S3_ACCESS_KEY_ID,
-    secret_access_key=settings.S3_SECRET_ACCESS_KEY,
-    endpoint_url=settings.S3_ENDPOINT_URL,
-)
