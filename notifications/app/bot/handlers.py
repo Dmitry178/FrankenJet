d3fbaa1818fd120316726147c73a3001c2427a61
Dmitry <dodegov@smarthomecrafters.ai>
2025-09-29 22:11:18 +0300

    Добавлены обработчики команд бота

---
+++ notifications/app/bot/handlers.py
@@ -0,0 +1,24 @@
+""" Обработчики команд """
+
+from aiogram import types, Router
+from aiogram.filters import Command
+
+from app.core.logs import bot_logger
+
+handlers_router = Router()
+
+
+@handlers_router.message(Command("id"))
+async def id_handler(message: types.Message):
+    """
+    Обработчик команды /id
+    """
+
+    user_id = message.from_user.id
+    msg = f"Ваш ID: *{user_id}*"
+
+    try:
+        await message.answer(msg)
+
+    except Exception as ex:
+        bot_logger.exception(ex)
