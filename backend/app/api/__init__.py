de8a055b746322a62f9f3450751613c112d63ea9
Dmitry <dodegov@smarthomecrafters.ai>
2025-09-29 21:39:13 +0300

    Добавлен роутер для тестирования бота уведомлений

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -4,6 +4,7 @@ from app.api.aircraft import aircraft_router
 from app.api.app import app_router
 from app.api.articles import articles_router
 from app.api.auth import auth_router
+from app.api.bot import bot_router
 from app.api.bureaus import bureaus_router
 from app.api.countries import countries_router
 from app.api.designers import designers_router
@@ -12,6 +13,7 @@ from app.api.oauth2 import oauth_router
 from app.api.pages import pages_router
 
 router = APIRouter()
+
 router.include_router(app_router)
 router.include_router(auth_router)
 router.include_router(oauth_router)
@@ -22,3 +24,5 @@ router.include_router(countries_router)
 router.include_router(bureaus_router)
 router.include_router(designers_router)
 router.include_router(manufacturers_router)
+
+router.include_router(bot_router)  # для тестирования бота
