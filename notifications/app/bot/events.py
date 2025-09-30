80416ebb2fb93a382e6efa6e6efbf9b0071f9b3b
Dmitry <dodegov@smarthomecrafters.ai>
2025-09-30 23:21:40 +0300

    Переработана отправка сообщения в бэкенд через rabbit mq

--- notifications/app/bot/events.py
+++ notifications/app/bot/events.py
@@ -1,28 +1,58 @@
 """ События телеграм-бота """
 
+import json
+
 from aiogram.types import CallbackQuery
 
 from app.bot import bot, dp
-from app.broker.subscribers import publish_to_backend
-from app.core.config import bot_settings
+from app.broker.subscribers import broker
+from app.core.config import bot_settings, RMQ_BACKEND_QUEUE
+from app.core.logs import bot_logger
 
 
-async def start_bot():
+async def start_bot() -> None:
     """
     Сообщение администраторам о запуске бота
     """
 
     text = f"<b>{bot_settings.APP_NAME}</b>. Бот запущен [{bot_settings.BUILD}]"
     await bot.send_message(chat_id=bot_settings.TELEGRAM_ADMIN_ID, text=text)
+    return None
 
 
-async def stop_bot():
+async def stop_bot() -> None:
     """
     Сообщение администраторам об остановке бота
     """
 
     text = f"<b>{bot_settings.APP_NAME}</b>. Бот остановлен [{bot_settings.BUILD}]"
     await bot.send_message(chat_id=bot_settings.TELEGRAM_ADMIN_ID, text=text)
+    return None
+
+
+async def send_response_and_update_message(message: dict, callback_query: CallbackQuery) -> None:
+    """
+    Отправка сообщения в RabbitMQ и удаление кнопок
+    """
+
+    try:
+        await broker.publish(json.dumps(message), queue=RMQ_BACKEND_QUEUE)
+        bot_logger.info("Решение по модерации отправлено в RabbitMQ")
+
+    except Exception as ex:
+        bot_logger.exception(f"Ошибка отправки сообщения: {ex}")
+        await bot.answer_callback_query(callback_query.id, text=f"Ошибка отправки ответа")
+        return
+
+    # удаление кнопок после отправки сообщения
+    await bot.edit_message_reply_markup(
+        chat_id=callback_query.message.chat.id,
+        message_id=callback_query.message.message_id,
+        reply_markup=None,
+    )
+    await bot.answer_callback_query(callback_query.id, text=f"Ответ отправлен")
+
+    return None
 
 
 @dp.callback_query()
@@ -32,37 +62,23 @@ async def handle_callback(callback_query: CallbackQuery):
     action = parts[0]
 
     if action == "admin_auth":
+        # отправка решения по аутентификации админа в бэкенде
         user_id = parts[1]
         result = parts[2]
-
-        # отправка результата аутентификации в бэкенд
-        publish_to_backend({
+        message = {
             "type": "admin_auth_response",
             "id": user_id,
             "result": result
-        })
-
-        # удаление кнопок после обработки
-        await bot.edit_message_reply_markup(
-            chat_id=callback_query.message.chat.id,
-            message_id=callback_query.message.message_id,
-            reply_markup=None,
-        )
-        await bot.answer_callback_query(callback_query.id, text=f"Отправлен ответ: {result}")
+        }
+        await send_response_and_update_message(message, callback_query)
 
     elif action == "moderation":
+        # отправка решения о модерации в бэкенд
         comment_id = parts[1]
         decision = parts[2]
-
-        # отправка решения о модерации в бэкенд
-        publish_to_backend({
+        message = {
             "type": "moderation_response",
             "comment_id": comment_id,
             "decision": decision
-        })
-        await bot.edit_message_reply_markup(
-            chat_id=callback_query.message.chat.id,
-            message_id=callback_query.message.message_id,
-            reply_markup=None
-        )
-        await bot.answer_callback_query(callback_query.id, text=f"Отправлен ответ: {decision}")
+        }
+        await send_response_and_update_message(message, callback_query)
