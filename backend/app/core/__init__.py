40488faee4fe3d6a50a95b35631838ca4f9a08a8
Dmitry <dodegov@smarthomecrafters.ai>
2025-09-30 23:18:38 +0300

    Убран лишний аргумент

--- backend/app/core/__init__.py
+++ backend/app/core/__init__.py
@@ -2,4 +2,4 @@ from app.config.env import settings
 from app.core.rmq_manager import RMQManager
 
 # инициализация RabbitMQ-менеджера
-rmq_manager = RMQManager(url=settings.RMQ_CONN, max_retries=3)
+rmq_manager = RMQManager(url=settings.RMQ_CONN)
