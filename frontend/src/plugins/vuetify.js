c7df1f88b3d914f26f7fe8c050234382c48197d2
Dmitry <dodegov@smarthomecrafters.ai>
2025-10-04 14:28:34 +0300

    Коррекция стилей

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -3,12 +3,10 @@ import vuetifyConfig from '@/vuetify.config'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles';
 
 export default createVuetify({
   ...vuetifyConfig,
   components,
   directives,
-  icons: {
-    defaultSet: 'mdi',
-  },
 })
