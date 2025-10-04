7e62ad15bfa2fd84c33c16c6897814a09a5d460c
Dmitry <dodegov@smarthomecrafters.ai>
2025-10-04 22:44:11 +0300

    Добавлен путь к S3 для относительных ссылок на изображения, получаемых из базы

--- backend/app/services/pages.py
+++ backend/app/services/pages.py
@@ -1,3 +1,4 @@
+from app.config.env import settings
 from app.core.db_manager import DBManager
 
 
@@ -16,12 +17,22 @@ class PagesService:
         context = {}
 
         # случайные статьи
-        columns = ["slug", "title", "summary"]
-        random_articles = await self.db.articles.select_random(columns=columns, limit=3, is_published=True) or []
-        context["articles"] = random_articles
+        random_articles = await self.db.articles.select_random_articles(limit=3) or []
+
+        # обработка изображений
+        processed_articles = [
+            {
+                **article,
+                "image_url": f"{settings.S3_ENDPOINT_URL}{article['image_url']}" if article["image_url"] else None
+            }
+            for article in random_articles
+        ]
 
         # случайные факты
         random_facts = await self.db.facts.select_random(limit=5) or []
+
+        # сборка контекста
+        context["articles"] = processed_articles
         context["facts"] = [item["fact"] for item in random_facts]
 
         return context
