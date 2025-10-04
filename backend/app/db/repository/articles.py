539656ea51b5e9638ce06fbc49c7316de7afa14b
Dmitry <dodegov@smarthomecrafters.ai>
2025-10-04 22:42:22 +0300

    Сделан отдельный метод получения случайных статей с изображением воздушного судна

--- backend/app/db/repository/articles.py
+++ backend/app/db/repository/articles.py
@@ -1,4 +1,6 @@
-from app.db.models import Articles
+from sqlalchemy import select, func, true
+
+from app.db.models import Articles, Aircraft
 from app.db.repository.base import BaseRepository
 
 
@@ -8,3 +10,20 @@ class ArticlesRepository(BaseRepository):
     """
 
     model = Articles
+
+    async def select_random_articles(self, limit: int = None):
+        """
+        Получение случайных статей
+        """
+
+        query = (
+            select(Articles.slug, Articles.title, Articles.summary, Aircraft.image_url)
+            .select_from(Articles)
+            .join(Aircraft, Aircraft.article_id == Articles.id)
+            .where(Articles.is_published == true())
+            .order_by(func.random())
+            .limit(limit)
+        )
+        result = await self.session.execute(query)
+
+        return result.mappings().all()
