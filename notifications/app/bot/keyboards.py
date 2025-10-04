3f5dddc11baf445b369cdc2eae8a0b2a24242437
Dmitry <dodegov@smarthomecrafters.ai>
2025-10-04 12:04:19 +0300

    Добавлен вариант с кнопкой logout пользователя

--- notifications/app/bot/keyboards.py
+++ notifications/app/bot/keyboards.py
@@ -5,11 +5,15 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 def get_admin_auth_keyboard(ip_address: str):
     builder = InlineKeyboardBuilder()
     builder.add(types.InlineKeyboardButton(
-        text="ОК ✅",
+        text="OK ✅",
         callback_data=f"admin_auth:{ip_address}:ok")
     )
     builder.add(types.InlineKeyboardButton(
-        text="Заблокировать 🚫",
+        text="Logout ❌",
+        callback_data=f"admin_auth:{ip_address}:logout")
+    )
+    builder.add(types.InlineKeyboardButton(
+        text="Block 🚫",
         callback_data=f"admin_auth:{ip_address}:block")
     )
     return builder.as_markup()
