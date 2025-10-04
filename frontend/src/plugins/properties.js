19630dc5db310749a24a7ce40b7f014a685191a7
Dmitry <dodegov@smarthomecrafters.ai>
2025-10-04 21:49:34 +0300

    Убрана ссылка на изображение по умолчанию из глобальных настроек

--- frontend/src/plugins/properties.js
+++ frontend/src/plugins/properties.js
@@ -4,5 +4,4 @@ const DEFAULT_IMAGE = '/aircraft.svg';
 
 export function setupProperties(app) {
   app.config.globalProperties.$axios = axios;
-  app.config.globalProperties.$defaultImage = DEFAULT_IMAGE;
 }
