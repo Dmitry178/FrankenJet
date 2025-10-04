6e70075334cb4fb967a656294415ebaf858b52b2
Dmitry <dodegov@smarthomecrafters.ai>
2025-10-04 12:00:40 +0300

    Добавлен запуск скриптов инициализации данных и S3

--- backend/scripts/entrypoint.sh
+++ backend/scripts/entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -e
 
-# Запуск скрипта инициализации
+# Запуск скрипта инициализации S3
+python /code/scripts/init_s3.py
+
+# Запуск скрипта инициализации базы
 python /code/scripts/init_db.py
+
+# Запуск скрипта инициализации данных
+python /code/scripts/init_data.py
 
 # Запуск FastAPI
 exec uvicorn main:app --host 0.0.0.0 --port 8000 --forwarded-allow-ips=* --proxy-headers --no-server-header
