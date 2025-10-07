dfa76cc0de0c63e0daba3e51bd2918a67dca1f3f
Dmitry <dodegov@smarthomecrafters.ai>
2025-10-07 15:22:19 +0300

    Исправлен порт, добавлен комментарий

--- backend/main.py
+++ backend/main.py
@@ -70,12 +70,18 @@ async def run_api():
     Запуск API
     """
 
-    config = uvicorn.Config("main:app", host="0.0.0.0", port=8111, reload=True)
+    config = uvicorn.Config("main:app", host="0.0.0.0", port=8000, reload=True)
     server = uvicorn.Server(config)
     await server.serve()
 
 
 async def main():
+    """
+    Запуск API совместно с RabbitMQ consumers для локальной разработки.
+    В production запускать по отдельности: uvicorn (main.py) и cкрипт запуска консьюмера
+    /backend/app/consumers/startup.py
+    """
+
     # обработка сигналов
     loop = asyncio.get_running_loop()
     for sig in (signal.SIGTERM, signal.SIGINT):
