bbc68b7cf8c19d22fad6bbea55704739e63d1c06
Dmitry <dm1398@yandex.ru>
2025-10-07 22:59:23 +0300

    Добавлено кэширование настроек приложения в GET /settings

--- backend/app/api/app.py
+++ backend/app/api/app.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from app.core import cache_manager
 from app.core.logs import logger
 from app.services.app import AppServices
 from app.types import status_ok, status_error
@@ -8,6 +9,7 @@ app_router = APIRouter(tags=["App"])
 
 
 @app_router.get("/settings", summary="Настройки приложения")
+@cache_manager.cached(ttl=3600)
 async def get_app_settings():
     """
     Вывод настроек приложения
