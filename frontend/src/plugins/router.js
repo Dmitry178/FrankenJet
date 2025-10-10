171cc88a3c9b5eed7a718e08f739ce5ba5147e14
Dmitry <dm1398@yandex.ru>
2025-10-10 12:31:20 +0300

    Добавлен Search.vue в роутер

--- frontend/src/plugins/router.js
+++ frontend/src/plugins/router.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import { useAuthStore } from "@/stores/auth.js";
 import Home from '../components/Home.vue';
 import Login from '../components/Login.vue';
 import AuthGoogle from '../components/LoginGoogle.vue';
 import Register from '../components/Register.vue';
 import ResetPassword from '../components/ResetPassword.vue';
 import Article from '../components/Article.vue';
-import { useAuthStore } from "@/stores/auth.js";
+import Search from '../components/Search.vue';
 
 const routes = [
   { path: '/', component: Home, name: 'Home' },
@@ -14,6 +15,7 @@ const routes = [
   { path: '/register', component: Register, name: 'Register' },
   { path: '/reset', component: ResetPassword, name: 'ResetPassword' },
   { path: '/articles/:slug', component: Article, name: 'Article' },
+  { path: '/search', component: Search, name: 'Search' },
 ];
 
 export function setupRouter() {
