72165ea205e81910b92289f14c0abac35decd902
Dmitry <dm1398@yandex.ru>
2025-10-10 12:03:39 +0300

    Добавлена инициация менеджера ElasticSearch

--- backend/app/core/__init__.py
+++ backend/app/core/__init__.py
@@ -1,5 +1,6 @@
 from app.config.env import settings
 from app.core.cache_manager import CacheManager
+from app.core.es_manager import ESManager
 from app.core.http_manager import HTTPManager
 from app.core.rmq_manager import RMQManager
 
@@ -8,6 +9,9 @@ rmq_manager = RMQManager(url=settings.RMQ_CONN)
 
 # инициализация Cache-менеджера на основе Fastapi-cache2
 cache_manager = CacheManager(url=settings.REDIS_URL, password=settings.REDIS_PASSWORD)
+
+# инициализация ElasticSearch
+es_manager = ESManager(url=settings.ELASTICSEARCH_URL)
 
 # инициализация AIOHTTP менеджер сессий
 http_manager = HTTPManager()
