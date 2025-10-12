fb783ed6b35c8dd4c046fa3b6f87ab77abca3599
Dmitry <dm1398@yandex.ru>
2025-10-12 17:44:55 +0300

    Добавлены схемы SuccessResponse и ErrorResponse

--- backend/app/schemas/api.py
+++ backend/app/schemas/api.py
@@ -1,4 +1,20 @@
 from pydantic import BaseModel
+from typing import Any
+
+from app.types import status_ok, status_error
+
+
+class SuccessResponse(BaseModel):
+    status: str = status_ok.get("status")
+    data: Any | None = None
+
+
+class ErrorResponse(BaseModel):
+    status: str = status_error.get("status")
+    detail: str | None = None
+
+
+ApiResponse = SuccessResponse | ErrorResponse  # стандартный ответ от API
 
 
 class SClientInfo(BaseModel):
