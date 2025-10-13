c61ca7255fa3eea3bf9c52efaa806032eb7fc02c
Dmitry <dm1398@yandex.ru>
2025-10-13 14:22:34 +0300

    Схемы конструкторских бюро вынесены в отдельный модуль

---
+++ backend/app/schemas/design_bureaus.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, Field
+
+
+class SDesignBureaus(BaseModel):
+    """
+    Схема конструкторских бюро
+    """
+
+    country_id: str = Field(..., min_length=2, max_length=2, description="Двухбуквенный ISO-код страны")
+    name: str = Field(..., max_length=32, description="Название конструкторского бюро")
+    description: str = Field(..., description="Описание конструкторского бюро")
