01fdca44d49a37ff81c4d0e52d69c23bca66ec2c
Dmitry <dm1398@yandex.ru>
2025-10-13 14:23:19 +0300

    Схема модели конструкторов вынесена в отдельный модуль

---
+++ backend/app/schemas/designers.py
@@ -0,0 +1,17 @@
+from datetime import date
+
+from pydantic import BaseModel, Field
+
+
+class SDesigners(BaseModel):
+    """
+    Схема конструкторов
+    """
+
+    country_id: str = Field(..., min_length=2, max_length=2, description="Двухбуквенный ISO-код страны")
+    name: str = Field(..., max_length=32, description="Имя конструктора")
+    birth_date: date | None = Field(None)
+    death_date: date | None = Field(None)
+    known_for: str | None = Field(None, description="Чем знаменит конструктор")
+    biography: str = Field(..., description="Биография")
+    image_url: str | None = Field(None, description="URL основного изображения")
