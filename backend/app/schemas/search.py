24fcfe1ebaa6f297fc54c9673122be4dddd7fb2c
Dmitry <dm1398@yandex.ru>
2025-10-13 21:37:10 +0300

    Исправлена опечатка

--- backend/app/schemas/search.py
+++ backend/app/schemas/search.py
@@ -6,7 +6,7 @@ class SSearch(BaseModel):
     Схема поискового запроса
     """
 
-    query: str | None = Field(None, max_length=256, description="Поисковый запрос"),
-    categories: list | None = Field(None, max_length=128, description="Список категорий статей"),
-    page: int = Field(1, ge=1, description="Номер страницы"),
-    per_page: int = Field(20, ge=1, le=100, description="Количество статей на странице"),
+    query: str | None = Field(None, max_length=256, description="Поисковый запрос")
+    categories: list | None = Field(None, max_length=128, description="Список категорий статей")
+    page: int = Field(1, ge=1, description="Номер страницы")
+    per_page: int = Field(20, ge=1, le=100, description="Количество статей на странице")
