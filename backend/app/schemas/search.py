d6eef5c6938107e91f910ec06761ea652454810c
Dmitry <dm1398@yandex.ru>
2025-10-13 17:08:24 +0300

    Добавлена схема поискового запроса

---
+++ backend/app/schemas/search.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel, Field
+
+
+class SSearch(BaseModel):
+    """
+    Схема поискового запроса
+    """
+
+    query: str | None = Field(None, max_length=256, description="Поисковый запрос"),
+    categories: list | None = Field(None, max_length=128, description="Список категорий статей"),
+    page: int = Field(1, ge=1, description="Номер страницы"),
+    per_page: int = Field(20, ge=1, le=100, description="Количество статей на странице"),
