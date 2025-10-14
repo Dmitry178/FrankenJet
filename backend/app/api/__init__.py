beb238cc47951a016b2265b8ae0b4c82d6e2010a
Dmitry <dm1398@yandex.ru>
2025-10-14 14:49:51 +0300

    Добавлен admin_router

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from app.api.admin import admin_router
 from app.api.aircraft import aircraft_router
 from app.api.app import app_router
 from app.api.articles import articles_router
@@ -17,6 +18,7 @@ from app.api.tags import tags_router
 router = APIRouter()
 
 router.include_router(app_router)
+router.include_router(admin_router)
 router.include_router(auth_router)
 router.include_router(oauth_router)
 router.include_router(search_router)
