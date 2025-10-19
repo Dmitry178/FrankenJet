b3a46deff008c95ecf77e82b4e143e701c8ecf00
Dmitry <dm1398@yandex.ru>
2025-10-20 00:59:19 +0300

    Убран запуск скриптов инициализации, если режим production

--- backend/scripts/entrypoint.sh
+++ backend/scripts/entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -e
 
-# Запуск скрипта инициализации S3
-python /code/scripts/init_s3.py
+if [ "$APP_MODE" != "production" ]; then
+  # Запуск скрипта инициализации S3
+  python /code/scripts/init_s3.py
 
-# Запуск скрипта инициализации базы
-python /code/scripts/init_db.py
+  # Запуск скрипта инициализации базы
+  python /code/scripts/init_db.py
 
-# Запуск скрипта инициализации данных
-python /code/scripts/init_data.py
+  # Запуск скрипта инициализации данных
+  python /code/scripts/init_data.py
+fi
 
 # Запуск FastAPI
 exec uvicorn main:app --host 0.0.0.0 --port 8000 --forwarded-allow-ips=* --proxy-headers --no-server-header
