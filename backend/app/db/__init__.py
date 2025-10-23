b8a4d402fd19973ef6c88074d69dc14b66142ef4
Dmitry <dm1398@yandex.ru>
2025-10-23 19:14:50 +0300

    Вынесен project_metadata из types.py

--- backend/app/db/__init__.py
+++ backend/app/db/__init__.py
@@ -1,15 +1,17 @@
-from sqlalchemy import NullPool
+from sqlalchemy import NullPool, MetaData
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 
 from app.config.env import settings
-from app.db.types import project_metadata, annotation_map
+from app.db.types import annotation_map
 
 engine = create_async_engine(settings.db_url, echo=False)
 engine_null_pool = create_async_engine(settings.db_url, poolclass=NullPool)
 
 async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
 async_session_maker_null_pool = async_sessionmaker(bind=engine_null_pool, expire_on_commit=False)
+
+project_metadata = MetaData()
 
 
 class Base(DeclarativeBase):
