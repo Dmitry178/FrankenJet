65c2d6d0bfb5903e291c25861d0296a44e26870e
Dmitry <dm1398@yandex.ru>
2025-10-25 15:17:31 +0300

    Убран роутер производителей

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -9,7 +9,6 @@ from app.api.bot import bot_router
 from app.api.bureaus import bureaus_router
 from app.api.countries import countries_router
 from app.api.designers import designers_router
-from app.api.manufacturers import manufacturers_router
 from app.api.oauth2 import oauth_router
 from app.api.pages import pages_router
 from app.api.search import search_router
@@ -29,5 +28,4 @@ router.include_router(aircraft_router)
 router.include_router(countries_router)
 router.include_router(bureaus_router)
 router.include_router(designers_router)
-router.include_router(manufacturers_router)
 router.include_router(bot_router)  # для тестирования бота
