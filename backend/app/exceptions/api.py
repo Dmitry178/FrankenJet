f01c550c2ff5625de6adc82d106dd1ddcd8b6e3c
Dmitry <dm1398@yandex.ru>
2025-10-26 21:58:35 +0300

    Добавлено исключение для ситуации изменения несуществующей записи

--- backend/app/exceptions/api.py
+++ backend/app/exceptions/api.py
@@ -8,6 +8,11 @@ from app.types import status_error
 
 forbidden_403 = HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Forbidden")
 
+record_was_not_found_404 = JSONResponse(
+    status_code=status.HTTP_404_NOT_FOUND,
+    content={**status_error, "detail": "Запись не найдена"}
+)
+
 http_error_500 = JSONResponse(
     status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
     content={**status_error, "detail": "Ошибка сервиса"}
