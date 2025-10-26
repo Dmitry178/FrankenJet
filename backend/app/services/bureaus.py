e10bc1c169ddb8d0ee4f0f20dc70a72722b745c5
Dmitry <dm1398@yandex.ru>
2025-10-26 15:31:37 +0300

    Добавлен docstring

--- backend/app/services/bureaus.py
+++ backend/app/services/bureaus.py
@@ -1,9 +1,13 @@
+"""
+Сервисный слой конструкторских бюро. В данной итерации проекта не используются.
+"""
+
 from uuid import UUID
 
 from app.core.db_manager import DBManager
 from app.decorators.db_errors import handle_basic_db_errors
+from app.schemas.bureaus import SDesignBureaus
 from app.schemas.countries import SCountriesFilters
-from app.schemas.design_bureaus import SDesignBureaus
 
 
 class BureausServices:
@@ -20,7 +24,7 @@ class BureausServices:
         """
 
         filter_by = filters.model_dump(exclude_none=True) if filters else {}
-        return await self.db.aircraft.design_bureaus.select_all(filter_by)
+        return await self.db.design_bureaus.select_all(filter_by)
 
     @handle_basic_db_errors
     async def add_design_bureau(self, data: SDesignBureaus):
@@ -28,7 +32,7 @@ class BureausServices:
         Добавление карточки конструкторского бюро
         """
 
-        return await self.db.aircraft.design_bureaus.insert_one(data)
+        return await self.db.design_bureaus.insert_one(data)
 
     @handle_basic_db_errors
     async def edit_design_bureau(self, design_bureaus_id: UUID, data: SDesignBureaus, exclude_unset=False):
@@ -36,7 +40,7 @@ class BureausServices:
         Редактирование карточки конструкторского бюро
         """
 
-        return await self.db.aircraft.design_bureaus.update(
+        return await self.db.design_bureaus.update(
             data,
             id=design_bureaus_id,
             exclude_unset=exclude_unset,
@@ -49,4 +53,4 @@ class BureausServices:
         Удаление карточки конструкторского бюро
         """
 
-        return await self.db.aircraft.design_bureaus.delete(id=design_bureaus_id)
+        return await self.db.design_bureaus.delete(id=design_bureaus_id)
