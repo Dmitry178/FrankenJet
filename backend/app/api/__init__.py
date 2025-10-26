d928a85045a38ae42a3acfd74e0c11e39479d077
Dmitry <dm1398@yandex.ru>
2025-10-26 15:09:51 +0300

    Убраны роутеры конструкторских бюро и конструкторов

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -6,9 +6,7 @@ from app.api.app import app_router
 from app.api.articles import articles_router
 from app.api.auth import auth_router
 from app.api.bot import bot_router
-from app.api.bureaus import bureaus_router
 from app.api.countries import countries_router
-from app.api.designers import designers_router
 from app.api.oauth2 import oauth_router
 from app.api.pages import pages_router
 from app.api.search import search_router
@@ -26,6 +24,4 @@ router.include_router(articles_router)
 router.include_router(tags_router)
 router.include_router(aircraft_router)
 router.include_router(countries_router)
-router.include_router(bureaus_router)
-router.include_router(designers_router)
 router.include_router(bot_router)  # для тестирования бота
