7b385142ac71c552bbd0c85e3d7afbd7a097cb11
Dmitry <dm1398@yandex.ru>
2025-10-26 15:10:01 +0300

    Убраны модели конструкторских бюро и конструкторов

--- backend/app/db/models/__init__.py
+++ backend/app/db/models/__init__.py
@@ -2,12 +2,6 @@
 
 from app.db.models.aircraft import (
     Aircraft,
-    DesignBureaus,
-    DesignersBureausAssociation,
-)
-from app.db.models.designers import (
-    AircraftDesignersAssociation,
-    Designers,
 )
 from app.db.models.articles import (
     Articles,
@@ -31,13 +25,9 @@ from app.db.models.users import (
 
 __all__ = [
     "Aircraft",
-    "AircraftDesignersAssociation",
     "Articles",
     "ArticlesTagsAssociation",
     "Countries",
-    "DesignBureaus",
-    "Designers",
-    "DesignersBureausAssociation",
     "Facts",
     "RefreshTokens",
     "Roles",
