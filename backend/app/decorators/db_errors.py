232a719635d6ea18e673bacab567d65420129816
Dmitry <dm1398@yandex.ru>
2025-10-27 22:28:36 +0300

    Изменение определения ошибки UniqueViolationError, добавлены логи

--- backend/app/decorators/db_errors.py
+++ backend/app/decorators/db_errors.py
@@ -1,15 +1,15 @@
 from functools import wraps
 
-import asyncpg
 from sqlalchemy.exc import IntegrityError, SQLAlchemyError, MultipleResultsFound, NoResultFound
 
+from app.core.logs import logger
 from app.exceptions.base import DatabaseServiceError, ServiceError, DatabaseUniqueFieldError, BaseCustomException, \
     DatabaseMultipleResultsError, DatabaseNoResultError
 
 
 def handle_basic_db_errors(func):
     """
-    Декоратор для обработки ошибок IntegrityError и SQLAlchemyError
+    Декоратор для обработки основных ошибок базы данных
     """
 
     @wraps(func)
@@ -21,22 +21,33 @@ def handle_basic_db_errors(func):
             raise ex
 
         except NoResultFound as ex:
+            logger.warning(ex)
             raise DatabaseNoResultError from ex
 
         except MultipleResultsFound as ex:
+            logger.warning(ex)
             raise DatabaseMultipleResultsError from ex
 
         except IntegrityError as ex:
-            orig = ex.orig
-            if isinstance(orig, asyncpg.exceptions.UniqueViolationError):
+            if getattr(ex.orig, 'pgcode', None) == "23505":
+                logger.warning(ex)
                 raise DatabaseUniqueFieldError from ex
             else:
+                logger.error(ex)
                 raise DatabaseServiceError from ex
 
         except SQLAlchemyError as ex:
+            logger.error(ex)
             raise DatabaseServiceError from ex
 
+        except AttributeError as ex:
+            # ошибка кода
+            logger.exception(ex)
+            raise ServiceError from ex
+
         except Exception as ex:
+            # прочие ошибки
+            logger.exception(ex)
             raise ServiceError from ex
 
     return wrapper
