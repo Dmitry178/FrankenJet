3eaa3460060bc8e9b1337d2acb20b61285a8830e
Dmitry <dm1398@yandex.ru>
2025-10-29 12:52:06 +0300

    Добавлен роутер фактов об авиации

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -7,6 +7,7 @@ from app.api.articles import articles_router
 from app.api.auth import auth_router
 from app.api.bot import bot_router
 from app.api.countries import countries_router
+from app.api.facts import facts_router
 from app.api.oauth2 import oauth_router
 from app.api.pages import pages_router
 from app.api.search import search_router
@@ -21,6 +22,7 @@ router.include_router(oauth_router)
 router.include_router(search_router)
 router.include_router(pages_router)
 router.include_router(articles_router)
+router.include_router(facts_router)
 router.include_router(tags_router)
 router.include_router(aircraft_router)
 router.include_router(countries_router)
