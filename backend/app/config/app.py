872340a940abcf0bb775ebdba326b5cdd8773f0a
Dmitry <dm1398@yandex.ru>
2025-10-29 12:52:53 +0300

    Добавлен nosec B105 для исключения ложно-положительного срабатывания проверки на безопасность

--- backend/app/config/app.py
+++ backend/app/config/app.py
@@ -9,10 +9,10 @@ JWT_REFRESH_EXPIRE_MINUTES: int = 60 * 24 * 30  # время жизни refresh-
 
 # OAuth2
 OAUTH2_GOOGLE_URL = "https://accounts.google.com/o/oauth2/v2/auth"
-OAUTH2_GOOGLE_TOKEN_URL = "https://oauth2.googleapis.com/token"
+OAUTH2_GOOGLE_TOKEN_URL = "https://oauth2.googleapis.com/token"  # nosec B105
 OAUTH2_GOOGLE_REDIRECT_URL = "http://localhost:3000/auth/google"  # TODO: переделать с учётом test и production
 OAUTH2_VK_URL = "https://id.vk.com/authorize"
-OAUTH2_VK_TOKEN_URL = "https://api.vk.com/method/auth.exchangeSilentAuthToken"
+OAUTH2_VK_TOKEN_URL = "https://api.vk.com/method/auth.exchangeSilentAuthToken"  # nosec B105
 OAUTH2_VK_REDIRECT_URL = "http://localhost/auth/vk"  # TODO: переделать с учётом test и production
 # VK при использовании http поддерживает только localhost (без порта)  # TODO учесть этот момент
 
