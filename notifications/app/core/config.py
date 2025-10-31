7af1192ce24ed02369c097328e46332119002c00
Dmitry <dm1398@yandex.ru>
2025-10-31 19:20:18 +0300

    Исправлены типы переменных окружения, переименованы очереди RMQ

--- notifications/app/core/config.py
+++ notifications/app/core/config.py
@@ -1,13 +1,9 @@
 from enum import Enum
-
-from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 # очереди RabbitMQ
-RMQ_NOTIFICATIONS_QUEUE: str = "notification"  # уведомления бота
-RMQ_ADMIN_AUTH_QUEUE: str = "admin_auth"  # уведомление об аутентификации админа
-RMQ_MODERATION_QUEUE: str = "moderation"  # комментарии на модерацию
-RMQ_BACKEND_QUEUE: str = "backend_bot"  # очередь отправки сообщения в бэкенд
+RMQ_FJ_OUTPUT_QUEUE = "fj_output"  # очередь для приёма сообщений ботом от бэкенда
+RMQ_FJ_INPUT_QUEUE = "fj_input"  # очередь для приёма сообщения бэкендом от бота
 
 
 class BotAppMode(str, Enum):
@@ -25,15 +21,15 @@ class BotSettings(BaseSettings):
     Настройки приложения
     """
 
-    APP_NAME: str = "Franken Jet Notification Bot"  # название приложения
+    APP_NAME: str = "FJ Notification Bot"  # название приложения
     APP_MODE: BotAppMode = BotAppMode.local  # режим работы приложения
     BUILD: str = "0"  # билд приложения
 
     RMQ_CONN: str | None = None  # строка подключения к RabbitMQ
 
     # telegram id учётной записи админа (на данном этапе - единичная запись)
-    TELEGRAM_ADMIN_ID: int = Field(..., gt=0)
-    TELEGRAM_API_TOKEN: str = Field(..., min_length=1)
+    TELEGRAM_ADMIN_ID: int | str
+    TELEGRAM_API_TOKEN: str
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
