7d0163c44f7922eceafbf28440e07970134a5a9c
Dmitry <dm1398@yandex.ru>
2025-11-01 14:24:53 +0300

    Незначительные изменения

--- notifications/app/core/logs.py
+++ notifications/app/core/logs.py
@@ -5,7 +5,7 @@ from app.core.config import bot_settings
 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     handlers=[logging.StreamHandler(sys.stdout)]
 )
 
