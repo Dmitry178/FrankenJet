e11c67db55c6d1793a80de903b9a1d0d7a2be3f0
Dmitry <dm1398@yandex.ru>
2025-11-01 14:24:40 +0300

    Добавлен обработчик команды /start

--- notifications/app/bot/handlers.py
+++ notifications/app/bot/handlers.py
@@ -3,9 +3,27 @@
 from aiogram import types, Router
 from aiogram.filters import Command
 
+from app.core.config import bot_settings
 from app.core.logs import bot_logger
 
 handlers_router = Router()
+
+
+@handlers_router.message(Command("start"))
+async def id_handler(message: types.Message):
+    """
+    Обработчик команды /start, приветствие админа
+    """
+
+    if str(message.from_user.id) != str(bot_settings.TELEGRAM_ADMIN_ID):
+        return
+
+    try:
+        msg = f"Приветствую, <b>{message.from_user.first_name}</b>!"
+        await message.answer(msg)
+
+    except Exception as ex:
+        bot_logger.exception(ex)
 
 
 @handlers_router.message(Command("id"))
