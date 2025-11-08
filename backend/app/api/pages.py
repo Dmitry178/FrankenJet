579523b12d71936f085f23facacaed45e669e566
Dmitry <dm1398@yandex.ru>
2025-11-08 14:04:25 +0300

    Добавлен эндпоинт для получения информации для страницы статей

--- backend/app/api/pages.py
+++ backend/app/api/pages.py
@@ -4,14 +4,14 @@ from app.core import cache_manager
 from app.core.logs import logger
 from app.dependencies.db import DDB
 from app.exceptions.base import BaseCustomException
+from app.schemas.api import SuccessResponse
 from app.services.pages import PagesService
-from app.types import status_ok
 
 pages_router = APIRouter(prefix="/pages", tags=["Pages"])
 
 
 @pages_router.get("/home", summary="Информация для главной страницы")
-@cache_manager.cached(ttl=720)
+@cache_manager.cached(ttl=3600)
 async def home(db: DDB):
     """
     Получение информации для главной страницы
@@ -19,7 +19,22 @@ async def home(db: DDB):
 
     try:
         data = await PagesService(db).home()
-        return {**status_ok, "data": data}
+        return SuccessResponse(data=data)
+
+    except BaseCustomException as ex:
+        logger.exception(ex)
+        return ex.json_response
+
+
+@pages_router.get("/articles", summary="Информация для страницы статей")
+async def home(db: DDB):
+    """
+    Получение информации для страницы статей
+    """
+
+    try:
+        data = await PagesService(db).articles()
+        return SuccessResponse(data=data)
 
     except BaseCustomException as ex:
         logger.exception(ex)
