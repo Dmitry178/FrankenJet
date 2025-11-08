157b9f5724de24cfafe4cd0131eee746b3b10c00
Dmitry <dm1398@yandex.ru>
2025-11-08 14:04:46 +0300

    Добавлена обработка информации для страницы статей в сервисном слое

--- backend/app/services/pages.py
+++ backend/app/services/pages.py
@@ -1,5 +1,8 @@
+from enum import Enum
+
 from app.config.env import settings
 from app.core.db_manager import DBManager
+from app.db.models.aircraft import AircraftTypes, EngineTypes, AircraftStatus, AircraftPurpose
 from app.decorators.db_errors import handle_basic_db_errors
 
 
@@ -36,5 +39,49 @@ class PagesService:
         # сборка контекста
         context["articles"] = processed_articles
         context["facts"] = [item["fact"] for item in random_facts]
+
+        return context
+
+    @handle_basic_db_errors
+    async def articles(self) -> dict:
+        """
+        Информация для страницы статей
+        """
+
+        def enum_key_to_value(enum_class: type[Enum], key: str) -> str | None:
+            """
+            Преобразование ключа enum в его значение (value)
+            """
+
+            try:
+                value = getattr(enum_class, key)
+                return str(value)
+            except ValueError:
+                return None
+
+        rows = await self.db.tags.count_tags()
+
+        tags = {}
+        for row in rows:
+            tag_type = row["type"]
+            tag_value = row["value"]
+
+            if tag_type == "aircraft_type":
+                tag_value = enum_key_to_value(AircraftTypes, tag_value)
+            elif tag_type == "engine_type":
+                tag_value = enum_key_to_value(EngineTypes, tag_value)
+            elif tag_type == "status":
+                tag_value = enum_key_to_value(AircraftStatus, tag_value)
+            elif tag_type == "aircraft_purpose":
+                tag_value = enum_key_to_value(AircraftPurpose, tag_value)
+
+            if tag_value is None:
+                continue
+
+            if tag_type not in tags:
+                tags[tag_type] = []
+            tags[tag_type].append(tag_value)
+
+        context = {"tags": tags}
 
         return context
