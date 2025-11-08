8a7cf56da1a06d2d62917c0c9b09ced6e8c7b0b4
Dmitry <dm1398@yandex.ru>
2025-11-08 14:05:11 +0300

    Добавлен подсчёт тегов во всех статьях

--- backend/app/db/repository/tags.py
+++ backend/app/db/repository/tags.py
@@ -1,6 +1,6 @@
-from sqlalchemy import update
+from sqlalchemy import update, select, literal_column, func, union_all
 
-from app.db.models import Tags
+from app.db.models import Tags, Countries, Aircraft, Articles
 from app.db.repository.base import BaseRepository
 
 
@@ -25,3 +25,67 @@ class TagsRepository(BaseRepository):
         result = await self.session.execute(stmt)
         await self.session.commit()
         return result.scalar()
+
+    async def count_tags(self):
+        """
+        Подсчёт тегов во всех статьях
+        """
+
+        base_cte = (
+            select(
+                Countries.name.label("country"),
+                Aircraft.aircraft_type,
+                Aircraft.aircraft_purpose,
+                Aircraft.engine_type,
+                Aircraft.status,
+            )
+            .select_from(Articles)
+            .join(Aircraft, Aircraft.article_id == Articles.id)
+            .join(Countries, Countries.id == Aircraft.country_id)
+            .where(
+                Articles.is_published.is_(True),
+                Articles.is_archived.is_(False),
+            )
+        ).cte("base_cte")
+
+        country_counts = select(
+            literal_column("'country'").label("type"),
+            base_cte.c.country.label("value"),
+            func.count().label("count")
+        ).where(base_cte.c.country.isnot(None)).group_by(base_cte.c.country)
+
+        aircraft_type_counts = select(
+            literal_column("'aircraft_type'").label("type"),
+            base_cte.c.aircraft_type.label("value"),
+            func.count().label("count")
+        ).where(base_cte.c.aircraft_type.isnot(None)).group_by(base_cte.c.aircraft_type)
+
+        aircraft_purpose_counts = select(
+            literal_column("'aircraft_purpose'").label("type"),
+            base_cte.c.aircraft_purpose.label("value"),
+            func.count().label("count")
+        ).where(base_cte.c.aircraft_purpose.isnot(None)).group_by(base_cte.c.aircraft_purpose)
+
+        engine_type_counts = select(
+            literal_column("'engine_type'").label("type"),
+            base_cte.c.engine_type.label("value"),
+            func.count().label("count")
+        ).where(base_cte.c.engine_type.isnot(None)).group_by(base_cte.c.engine_type)
+
+        status_counts = select(
+            literal_column("'status'").label("type"),
+            base_cte.c.status.label("value"),
+            func.count().label("count")
+        ).where(base_cte.c.status.isnot(None)).group_by(base_cte.c.status)
+
+        union_all_query = [
+            country_counts,
+            aircraft_type_counts,
+            aircraft_purpose_counts,
+            engine_type_counts,
+            status_counts,
+        ]
+
+        query = (union_all(*union_all_query).select())
+
+        return (await self.session.execute(query)).mappings().all()
