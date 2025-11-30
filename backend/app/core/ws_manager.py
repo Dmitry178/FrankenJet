e7af4f62a17b0287162a82d2a472b57125adf0e9
Dmitry <dm1398@yandex.ru>
2025-11-30 22:07:30 +0300

    Переписана логика работы websocket под чат-бот

--- backend/app/core/ws_manager.py
+++ backend/app/core/ws_manager.py
@@ -1,71 +1,65 @@
 """ Менеджер WebSocket """
 
+import json
+
 from fastapi import WebSocket, WebSocketDisconnect
 from typing import Dict, List
 from uuid import UUID
 
+from app.core.logs import logger
 
-class WSManager:
+
+class WSBotManager:
     def __init__(self):
-        # user_id -> {jti -> [WebSocket, ...]}
-        self.active_connections: Dict[int, Dict[UUID, List[WebSocket]]] = {}
+        # {chat_id: [WebSocket, ...]}
+        self.active_connections: Dict[UUID, List[WebSocket]] = {}
 
-    async def connect(self, user_id: int, jti: UUID, websocket: WebSocket):
+    async def connect(self, chat_id: UUID, websocket: WebSocket):
         await websocket.accept()
-        if user_id not in self.active_connections:
-            self.active_connections[user_id] = {}
-        if jti not in self.active_connections[user_id]:
-            self.active_connections[user_id][jti] = []
-        self.active_connections[user_id][jti].append(websocket)
 
-    def disconnect(self, user_id: int, jti: UUID, websocket: WebSocket):
-        if user_id in self.active_connections:
-            if jti in self.active_connections[user_id]:
+        # создание списка подключений для chat_id, если его нет
+        if chat_id not in self.active_connections:
+            self.active_connections[chat_id] = []
+
+        self.active_connections[chat_id].append(websocket)
+
+    def disconnect(self, chat_id: UUID, websocket: WebSocket):
+        if chat_id in self.active_connections:
+            try:
+                self.active_connections[chat_id].remove(websocket)
+            except ValueError:
+                logger.info("Подключение уже удалено")
+
+            # удаление chat_id, если список подключений пуст
+            if not self.active_connections[chat_id]:
+                del self.active_connections[chat_id]
+
+    async def send_message_to_chat(self, chat_id: UUID, message: str):
+        """
+        Отправка сообщения в chat_id
+        """
+
+        if chat_id in self.active_connections:
+            for websocket in self.active_connections[chat_id]:
                 try:
-                    self.active_connections[user_id][jti].remove(websocket)
-                except ValueError:
+                    data = {
+                        "type": "answer",
+                        "text": message,
+                    }
+                    await websocket.send_text(json.dumps(data))
+
+                except WebSocketDisconnect:
+                    # обработка отключившихся websocket в реальном времени
+                    self.disconnect(chat_id, websocket)
+
+    async def broadcast_message(self, message: str):
+        """
+        Отправка сообщения всем активным подключениям
+        """
+
+        for chat_connections in list(self.active_connections.values()):
+            for websocket in chat_connections:
+                try:
+                    await websocket.send_text(message)
+                except WebSocketDisconnect:
                     pass
-                if not self.active_connections[user_id][jti]:
-                    del self.active_connections[user_id][jti]
-            if not self.active_connections[user_id]:
-                del self.active_connections[user_id]
-
-    async def disconnect_jti(self, user_id: int, jti: UUID):
-        """
-        Отключение всех WebSocket-соединений по jti
-        """
-
-        if user_id in self.active_connections:
-            if jti in self.active_connections[user_id]:
-                for ws in self.active_connections[user_id][jti]:
-                    await ws.close(code=1000)
-                del self.active_connections[user_id][jti]
-            if not self.active_connections[user_id]:
-                del self.active_connections[user_id]
-
-    async def disconnect_user(self, user_id: int):
-        """
-        Отключение всех WebSocket-соединений пользователя
-        """
-
-        if user_id in self.active_connections:
-            for jti, ws_list in self.active_connections[user_id].items():
-                for ws in ws_list:
-                    await ws.close(code=1000)
-            del self.active_connections[user_id]
-
-    async def broadcast_to_user(self, user_id: int, message: str):
-        """
-        Отправка сообщения всем подключениям пользователя
-        """
-
-        if user_id in self.active_connections:
-            for jti, ws_list in self.active_connections[user_id].items():
-                for ws in ws_list:
-                    try:
-                        await ws.send_text(message)
-                    except WebSocketDisconnect:
-                        self.disconnect(user_id, jti, ws)
-
-
-ws_manager = WSManager()
