37b9c26491460cf74fcb6a8c6e4ea843f7b6e42e
Dmitry <dm1398@yandex.ru>
2025-11-30 22:06:35 +0300

    Переделан маршрут websocket под чат-бот

--- backend/app/api/websocket.py
+++ backend/app/api/websocket.py
@@ -1,18 +1,13 @@
 from fastapi import APIRouter, WebSocket
 from uuid import UUID
 
-from app.core.ws_manager import ws_manager
+from app.core import ws_manager
 from app.dependencies.db import DDB
-from app.services.websocket import WebSocketsAuthServices, WebSocketService
+from app.services.websocket import WebSocketService
 
 ws_router = APIRouter()
 
 
-@ws_router.websocket("/ws/{jti}")
-async def websocket_endpoint(websocket: WebSocket, jti: UUID, db: DDB):
-    if not (user_id := await WebSocketsAuthServices(db).auth_ws(jti)):
-        await websocket.close(code=1008)
-        return
-
-    await ws_manager.connect(user_id, jti, websocket)
-    await WebSocketService(ws_manager).handle_websocket_connection(websocket, user_id, jti)
+@ws_router.websocket("/ws/{chat_id}")
+async def websocket_endpoint(websocket: WebSocket, chat_id: UUID, db: DDB):
+    await WebSocketService(ws_manager, db).handle_websocket_connection(chat_id, websocket)
