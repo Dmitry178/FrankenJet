549990310c9a12c6013fe650243a032ded59b672
Dmitry <dm1398@yandex.ru>
2025-12-03 17:23:51 +0300

    Изменение типа полей в ChatHistory

--- backend/app/db/models/chatbot.py
+++ backend/app/db/models/chatbot.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 from app.db import Base
 from app.db.models.base import TimestampMixin
-from app.db.types import bool_false, str_24, str_32, str_512, bool_true
+from app.db.types import bool_false, str_24, str_32, str_512, bool_true, int_0
 
 
 class ChatBotSettings(Base):
@@ -15,15 +15,15 @@ class ChatBotSettings(Base):
     """
 
     __tablename__ = "settings"
-    __table_args__ = {"schema": "chat_bot"}
+    __table_args__ = {"schema": "chatbot"}
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     enabled: Mapped[bool_false]  # активирован ли чат-бот
     model: Mapped[str_32 | None]  # название LLM-модели
     scope: Mapped[str_32 | None]  # scope
     system_prompt: Mapped[str | None]  # общий системный промт
-    messages_per_user: Mapped[int | None]  # количество сообщений на пользователя
-    messages_per_day: Mapped[int | None]  # количество сообщений в день на всех пользователей
+    user_daily_tokens: Mapped[int | None]  # количество токенов в день на пользователя
+    total_daily_tokens: Mapped[int | None]  # количество токенов в день на всех пользователей
 
 
 class MessageIntent(str, enum.Enum):
@@ -51,7 +51,7 @@ class ChatHistory(Base, TimestampMixin):
     """
 
     __tablename__ = "chat"
-    __table_args__ = {"schema": "chat_bot"}
+    __table_args__ = {"schema": "chatbot"}
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     chat_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True))  # id чата
@@ -60,6 +60,6 @@ class ChatHistory(Base, TimestampMixin):
     intent: Mapped[str_24 | None] = mapped_column(Enum(MessageIntent, native_enum=False, length=16))  # тема сообщения
     prompt_tokens: Mapped[int | None]  # количество токенов промта
     completion_tokens: Mapped[int | None]  # количество токенов ответа модели
-    total_tokens: Mapped[int | None]  # количество токенов всего
-    precached_prompt_tokens: Mapped[int | None]  # количество токенов кэша (истории)
+    total_tokens: Mapped[int_0]  # количество токенов всего
+    precached_prompt_tokens: Mapped[int_0]  # количество токенов кэша (истории)
     is_active: Mapped[bool_true]  # запись активна
