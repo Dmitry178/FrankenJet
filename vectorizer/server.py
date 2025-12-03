51e8e6ffc2e1668daba00ed0973bd5f13493e858
Dmitry <dm1398@yandex.ru>
2025-12-03 17:31:45 +0300

    Добавлен скрипт запуска сервиса Vectorizer

---
+++ vectorizer/server.py
@@ -0,0 +1,10 @@
+from app.service import serve
+from app.logs import app_logger
+
+if __name__ == "__main__":
+    app_logger.info(f"Starting gRPC service")
+    try:
+        serve()
+
+    except KeyboardInterrupt:
+        app_logger("Stopping gRPC service")
