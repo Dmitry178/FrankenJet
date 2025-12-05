ac03f42c3d94688c478794f488a060a7e2af2981
Dmitry <dm1398@yandex.ru>
2025-12-05 15:35:12 +0300

    Добавлена директория к кэшу по умолчанию

--- vectorizer/app/config.py
+++ vectorizer/app/config.py
@@ -11,14 +11,11 @@ class Settings(BaseSettings):
 
     # Модель
     MODEL_NAME: str = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
-    FASTEMBED_CACHE_DIR: str | None = None
+    FASTEMBED_CACHE_DIR: str | None = "./models/cache"
 
     # Параметры gRPC сервера
     GRPC_HOST: str = "0.0.0.0"
     GRPC_PORT: int = 50051
-
-    # Максимальная длина текста
-    MAX_TEXT_LENGTH: int = 2048
 
 
 app_settings = Settings()
