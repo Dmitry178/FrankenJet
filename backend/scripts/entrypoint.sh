4a169d81855960629c3a99ea0ea29fbab2f52106
Dmitry <dm1398@yandex.ru>
2025-12-11 23:19:34 +0300

    Добавлен вариант развёртывания через gunicorn

--- backend/scripts/entrypoint.sh
+++ backend/scripts/entrypoint.sh
@@ -14,3 +14,4 @@ fi
 
 # Запуск FastAPI
 exec uvicorn main:app --host 0.0.0.0 --port 8000 --forwarded-allow-ips=* --proxy-headers --no-server-header
+# exec gunicorn main:app -w 4 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 --forwarded-allow-ips=* --proxy-headers --no-server-header
