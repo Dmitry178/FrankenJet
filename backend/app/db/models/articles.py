457c92a95ef864ea325eee2a71c52f45481f0cd3
Dmitry <dm1398@yandex.ru>
2026-01-03 15:17:15 +0300

    Добавлено поле sources, удалено view_count в модели Articles

--- backend/app/db/models/articles.py
+++ backend/app/db/models/articles.py
@@ -45,9 +45,9 @@ class Articles(Base, TimestampMixin):
     title: Mapped[str_64] = mapped_column(unique=True)  # заголовок статьи
     summary: Mapped[str_1024 | None]  # краткое описание статьи  # TODO: исправить размер строки
     content: Mapped[str] = mapped_column(Text)  # текст статьи
+    sources: Mapped[str | None] = mapped_column(Text)  # список источников
     meta_title: Mapped[str_512 | None]  # мета-информация для SEO (название)  # TODO: исправить размер строки
     meta_description: Mapped[str_1024 | None]  # мета-информация для SEO (описание)  # TODO: исправить размер строки
-    view_count: Mapped[int_0]  # количество просмотров страницы
     seo_keywords: Mapped[str_256 | None]  # ключевые слова SEO  # TODO: исправить размер строки
 
     is_published: Mapped[bool_false]  # статья опубликована
