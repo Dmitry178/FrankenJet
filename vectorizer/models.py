1aa327687de07fa170488fc5239c7d6447b1fa0e
Dmitry <dm1398@yandex.ru>
2026-01-03 15:14:52 +0300

    Добавлен вывод списка моделей для sentence_transformers

--- vectorizer/models.py
+++ vectorizer/models.py
@@ -1,6 +1,4 @@
-"""
-Вывод списка моделей, поддерживаемых FastEmbed
-"""
+""" Вывод списка моделей, поддерживаемых FastEmbed """
 
 from fastembed import TextEmbedding
 
@@ -11,3 +9,15 @@ from fastembed import TextEmbedding
 supported_models = TextEmbedding.list_supported_models()
 for model in supported_models:
     print(model)
+
+
+""" Вывод списка моделей, поддерживаемых Sentence Transformers """
+
+from huggingface_hub import list_models
+
+# поиск моделей, связанных с sentence-transformers
+models = list_models(filter="sentence-transformers", sort="downloads", direction=-1)
+
+print("Модели, помеченные как 'sentence-transformers':")
+for model in models:
+    print(f"- {model.id}")
