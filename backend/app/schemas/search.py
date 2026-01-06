72f8a50a898bda9ec151ccf2780075509e7d26b3
Dmitry <dm1398@yandex.ru>
2026-01-06 20:33:47 +0300

    Изменение названия поля в схеме поискового запроса

--- backend/app/schemas/search.py
+++ backend/app/schemas/search.py
@@ -9,4 +9,4 @@ class SSearch(BaseModel):
     query: str | None = Field(None, max_length=256, description="Поисковый запрос")
     categories: list | None = Field(None, max_length=128, description="Список категорий статей")
     page: int = Field(1, ge=1, description="Номер страницы")
-    per_page: int = Field(20, ge=1, le=100, description="Количество статей на странице")
+    page_size: int = Field(20, ge=1, le=100, description="Количество статей на странице")
