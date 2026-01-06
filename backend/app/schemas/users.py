27f57122c30634af6cdf0aa5bed5adef8fda25a5
Dmitry <dm1398@yandex.ru>
2026-01-06 20:35:29 +0300

    Добавлены схемы SUserProfile и SEditUserProfile

--- backend/app/schemas/users.py
+++ backend/app/schemas/users.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from pydantic import BaseModel, EmailStr
+from uuid import UUID
 
 
 class SUserInfo(BaseModel):
@@ -6,9 +8,9 @@ class SUserInfo(BaseModel):
     Схема информации о пользователе
     """
 
-    # TODO: убрать дублирование кода в схемах (пока оставлено для наглядности)
+    # дублирование кода в схемах оставлено для наглядности
 
-    id: int
+    id: UUID
     email: EmailStr
     full_name: str | None = None
     first_name: str | None = None
@@ -21,7 +23,7 @@ class SUserCreateOAuth2(BaseModel):
     Схема создания пользователя через OAuth2
     """
 
-    # TODO: убрать дублирование кода в схемах (пока оставлено для наглядности)
+    # дублирование кода в схемах оставлено для наглядности
 
     email: EmailStr
     full_name: str | None = None
@@ -37,3 +39,33 @@ class SUserCreateEmail(BaseModel):
 
     email: EmailStr
     hashed_password: str
+
+
+class SUserProfile(BaseModel):
+    """
+    Схема данных профиля пользователя
+    """
+
+    # дублирование кода в схемах оставлено для наглядности
+
+    id: UUID
+    email: str
+    full_name: str | None = None
+    first_name: str | None = None
+    last_name: str | None = None
+    picture: str | None = None
+    roles: list[str] | None = None
+    created_at: datetime | None = None
+    updated_at: datetime | None = None
+
+    model_config = {"from_attributes": True}
+
+
+class SEditUserProfile(BaseModel):
+    """
+    Схема для редактирования данных профиля пользователя
+    """
+
+    full_name: str | None = None
+    first_name: str | None = None
+    last_name: str | None = None
