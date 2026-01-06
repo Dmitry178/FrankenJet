ea002a0ca30324632da80e867c9b0b968dfefbab
Dmitry <dm1398@yandex.ru>
2026-01-06 20:35:05 +0300

    Добавлена пагинация в метод получения списка пользователей

--- backend/app/db/repository/users.py
+++ backend/app/db/repository/users.py
@@ -15,9 +15,9 @@ class UsersRepository(BaseRepository):
     model = Users
     # mapper = UsersDataMapper  # TODO: добавить mapper
 
-    async def get_user_with_roles(self, *filters, **filter_by):
+    async def get_user_with_roles(self, offset: int = 0, limit: int = None, *filters, **filter_by):
         """
-        Получение пользователя с его ролями
+        Получение пользователя/пользователей с ролями и пагинацией
         """
 
         query = (
@@ -27,9 +27,13 @@ class UsersRepository(BaseRepository):
             .options(selectinload(Users.roles))
         )
 
+        if offset is not None and limit is not None:
+            query = query.offset(offset).limit(limit)
+            return (await self.session.execute(query)).scalars().all()
+
         return (await self.session.execute(query)).scalars().one_or_none()
 
-    async def get_or_create_user(self, user_data: SUserCreateOAuth2) -> int:
+    async def get_or_create_user(self, user_data: SUserCreateOAuth2):
         """
         Создание пользователя, либо получение пользователя, если он уже создан
         """
@@ -42,7 +46,7 @@ class UsersRepository(BaseRepository):
         insert_stmt = (
             insert(Users)
             .values(**user_data.model_dump())
-            .on_conflict_do_nothing(index_elements=['email'])
+            .on_conflict_do_nothing(index_elements=["email"])
             .returning(Users.id)
         )
 
@@ -53,8 +57,8 @@ class UsersRepository(BaseRepository):
         )
 
         # CTE
-        cte = insert_stmt.cte('insert_cte')
-        id_stmt = select(cte.c.id).union_all(select_stmt).cte('id_cte')
+        cte = insert_stmt.cte("insert_cte")
+        id_stmt = select(cte.c.id).union_all(select_stmt).cte("id_cte")
 
         # итоговый запрос
         query = (
