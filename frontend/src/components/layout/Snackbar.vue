78edf6f65e0053f64df4c946ef95ab363521c0e8
Dmitry <dm1398@yandex.ru>
2026-01-06 20:46:16 +0300

    Добавлен компонент Snackbar

---
+++ frontend/src/components/layout/Snackbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-snackbar
+    v-model="snackbarState.show"
+    location="top"
+    :color="snackbarState.color"
+    :timeout="snackbarState.timeout"
+  >
+    {{ snackbarState.message }}
+  </v-snackbar>
+</template>
+
+<script>
+import { snackbarState } from '@/composables/useSnackbar';
+
+export default {
+  name: 'Snackbar',
+  setup() {
+    return {
+      snackbarState
+    };
+  }
+};
+</script>
