924e18cc5bd4274399dcf8d5af0f458545bec405
Dmitry <dm1398@yandex.ru>
2026-01-07 14:57:10 +0300

    Расширен класс вебсокета пользователей

--- backend/app/services/websocket.py
+++ backend/app/services/websocket.py
@@ -5,23 +5,38 @@ from uuid import UUID
 
 from app.core import chatbot_settings
 from app.core.db_manager import DBManager
-from app.core.ws_manager import WSBotManager
+from app.core.logs import logger
+from app.core.ws_manager import WSManager
 from app.dependencies.db import DDB
 from app.schemas.jti import SRefreshTokens
 from app.services.chatbot import ChatBotServices
-from scripts.init_db import logger
 
 
-class WebSocketsAuthServices:
+class WebSocketAuthService:
+    """
+    Websocket для работы с аутентифицированными пользователями
+    """
 
     db: DBManager | None
 
-    def __init__(self, db: DBManager | None = None) -> None:
+    def __init__(self, ws_manager: WSManager, db: DBManager | None = None) -> None:
+        self.ws_manager = ws_manager
         self.db = db
+
+    async def handle_websocket_connection(self, user_id: UUID, websocket: WebSocket):
+        await self.ws_manager.connect(user_id, websocket)
+
+        try:
+            while True:
+                data = await websocket.receive_text()
+                await self.process_websocket_message(user_id, data)
+
+        except WebSocketDisconnect:
+            self.ws_manager.disconnect(user_id, websocket)
 
     async def auth_ws(self, jti: UUID) -> UUID | None:
         """
-        Проверка аутентификации пользователя через веб-сокеты
+        Проверка аутентификации пользователя через websocket
         """
 
         token: SRefreshTokens = self.db.auth.refresh_tokens.select_one_or_none(jti=jti)
@@ -29,10 +44,25 @@ class WebSocketsAuthServices:
             return None
         return token.user_id
 
+    async def process_websocket_message(self, user_id: UUID, data: str):
+        """
+        Обработка входящих сообщений через websocket
+        """
 
-class WebSocketService:
+        try:
+            print(user_id, data)
+            ...
 
-    def __init__(self, ws_manager: WSBotManager, db: DDB):
+        except (json.JSONDecodeError, Exception) as ex:
+            logger.exception(ex)
+
+
+class WebSocketBotService:
+    """
+    Websocket чат-бота
+    """
+
+    def __init__(self, ws_manager: WSManager, db: DDB):
         self.ws_manager = ws_manager
         self.db = db
 
@@ -63,4 +93,4 @@ class WebSocketService:
                 ).proceed_user_message(chat_id, message)
 
         except (json.JSONDecodeError, Exception) as ex:
-            logger.error(ex)
+            logger.exception(ex)
