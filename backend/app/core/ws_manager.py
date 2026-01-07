4f2a2bd3042546fd332957ce53f4e093ddd7041d
Dmitry <dm1398@yandex.ru>
2026-01-07 14:57:41 +0300

    Корректировка нейминга класса WSManager

--- backend/app/core/ws_manager.py
+++ backend/app/core/ws_manager.py
@@ -9,38 +9,38 @@ from uuid import UUID
 from app.core.logs import logger
 
 
-class WSBotManager:
+class WSManager:
     def __init__(self):
-        # {chat_id: [WebSocket, ...]}
+        # {connection_id: [WebSocket, ...]}
         self.active_connections: Dict[UUID, List[WebSocket]] = {}
 
-    async def connect(self, chat_id: UUID, websocket: WebSocket):
+    async def connect(self, connection_id: UUID, websocket: WebSocket):
         await websocket.accept()
 
-        # создание списка подключений для chat_id, если его нет
-        if chat_id not in self.active_connections:
-            self.active_connections[chat_id] = []
+        # создание списка подключений для connection_id, если его нет
+        if connection_id not in self.active_connections:
+            self.active_connections[connection_id] = []
 
-        self.active_connections[chat_id].append(websocket)
+        self.active_connections[connection_id].append(websocket)
 
-    def disconnect(self, chat_id: UUID, websocket: WebSocket):
-        if chat_id in self.active_connections:
+    def disconnect(self, connection_id: UUID, websocket: WebSocket):
+        if connection_id in self.active_connections:
             try:
-                self.active_connections[chat_id].remove(websocket)
+                self.active_connections[connection_id].remove(websocket)
             except ValueError:
                 logger.info("Подключение уже удалено")
 
-            # удаление chat_id, если список подключений пуст
-            if not self.active_connections[chat_id]:
-                del self.active_connections[chat_id]
+            # удаление connection_id, если список подключений пуст
+            if not self.active_connections[connection_id]:
+                del self.active_connections[connection_id]
 
-    async def send_message_to_chat(self, chat_id: UUID, message: str):
+    async def send_message_to_connection(self, connection_id: UUID, message: str):
         """
-        Отправка сообщения в chat_id
+        Отправка сообщения в connection_id
         """
 
-        if chat_id in self.active_connections:
-            for websocket in self.active_connections[chat_id]:
+        if connection_id in self.active_connections:
+            for websocket in self.active_connections[connection_id]:
                 try:
                     data = {
                         "type": "answer",
@@ -50,15 +50,15 @@ class WSBotManager:
 
                 except WebSocketDisconnect:
                     # обработка отключившихся websocket в реальном времени
-                    self.disconnect(chat_id, websocket)
+                    self.disconnect(connection_id, websocket)
 
     async def broadcast_message(self, message: str):
         """
         Отправка сообщения всем активным подключениям
         """
 
-        for chat_connections in list(self.active_connections.values()):
-            for websocket in chat_connections:
+        for connections in list(self.active_connections.values()):
+            for websocket in connections:
                 try:
                     await websocket.send_text(message)
                 except WebSocketDisconnect:
