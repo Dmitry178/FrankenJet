4071e2d171e56d26b9455734ad11abdbe3d66603
Dmitry <dm1398@yandex.ru>
2026-01-07 14:54:58 +0300

    Добавлен эндпоинт вебсокета пользователей

--- backend/app/api/websocket.py
+++ backend/app/api/websocket.py
@@ -3,11 +3,16 @@ from uuid import UUID
 
 from app.core import ws_manager
 from app.dependencies.db import DDB
-from app.services.websocket import WebSocketService
+from app.services.websocket import WebSocketAuthService, WebSocketBotService
 
 ws_router = APIRouter()
 
 
-@ws_router.websocket("/ws/{chat_id}")
-async def websocket_endpoint(websocket: WebSocket, chat_id: UUID, db: DDB):
-    await WebSocketService(ws_manager, db).handle_websocket_connection(chat_id, websocket)
+@ws_router.websocket("/ws/auth/{user_id}")
+async def auth_websocket_endpoint(websocket: WebSocket, user_id: UUID, db: DDB):
+    await WebSocketAuthService(ws_manager, db).handle_websocket_connection(user_id, websocket)
+
+
+@ws_router.websocket("/ws/chat/{chat_id}")
+async def chat_websocket_endpoint(websocket: WebSocket, chat_id: UUID, db: DDB):
+    await WebSocketBotService(ws_manager, db).handle_websocket_connection(chat_id, websocket)
