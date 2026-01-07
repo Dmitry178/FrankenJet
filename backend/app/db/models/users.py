f7c55624f2576e5573d8b6c2fe301ded054d9a25
Dmitry <dm1398@yandex.ru>
2026-01-07 23:21:01 +0300

    Добавлена таблица проверочных кодов при регистрации пользователя

--- backend/app/db/models/users.py
+++ backend/app/db/models/users.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from sqlalchemy import String, Index
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
@@ -5,7 +7,7 @@ from typing import TYPE_CHECKING, List
 
 from app.db import Base
 from app.db.models.base import TimestampMixin
-from app.db.types import uid_pk, str_16, str_64, str_256, fk_role, fk_user_cascade
+from app.db.types import uid_pk, str_8, str_16, str_64, str_256, fk_role, fk_user_cascade, bool_false, fk_user
 
 if TYPE_CHECKING:
     from app.db.models import RefreshTokens
@@ -29,8 +31,10 @@ class Users(Base, TimestampMixin):
     first_name: Mapped[str_64 | None]  # имя
     last_name: Mapped[str_64 | None]  # фамилия
     picture: Mapped[str_256 | None]  # ссылка на фото профиля
+    is_active: Mapped[bool_false]  # блокировка пользователя
 
-    jti: Mapped[list["RefreshTokens"]] = relationship(back_populates="user")
+    refresh_tokens: Mapped[List["RefreshTokens"]] = relationship(back_populates="user")
+    verification_codes: Mapped[List["VerificationCodes"]] = relationship(back_populates="user")
     roles_association: Mapped[List["UsersRolesAssociation"]] = relationship(
         back_populates="user",
         cascade="all, delete-orphan",  # каскадно удаляются роли в ассоциативной таблице
@@ -79,3 +83,24 @@ class UsersRolesAssociation(Base):
 
     user: Mapped["Users"] = relationship(back_populates="roles_association")
     role: Mapped["Roles"] = relationship(back_populates="users_association")
+
+
+class VerificationCodes(Base, TimestampMixin):
+    """
+    Коды проверки при регистрации пользователей
+    """
+
+    __tablename__ = "verification_codes"
+    __table_args__ = (
+        Index("idx_user_id_expires_at", "user_id", "expires_at"),
+        {"schema": "users"}
+    )
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[fk_user] = mapped_column(index=True)
+    code: Mapped[str_8]
+    attempts: Mapped[int]
+    expires_at: Mapped[datetime] = mapped_column(index=True)
+    verified_at: Mapped[datetime]
+
+    user: Mapped["Users"] = relationship(back_populates="verification_codes")
