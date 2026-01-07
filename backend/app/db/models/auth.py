da15682355dea7f629d5addfc4a8bdb3add20b47
Dmitry <dm1398@yandex.ru>
2026-01-07 23:17:19 +0300

    Добавлены индексы полей модели RefreshTokens

--- backend/app/db/models/auth.py
+++ backend/app/db/models/auth.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import TYPE_CHECKING
 
 from app.db import Base
-from app.db.types import fk_user, uid, bool_false, datetime_now, str_256
+from app.db.types import fk_user, uid, datetime_now, str_256
 
 if TYPE_CHECKING:
     from app.db.models import Users
@@ -24,10 +24,9 @@ class RefreshTokens(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
 
-    user_id: Mapped[fk_user]
-    jti: Mapped[uid]
+    user_id: Mapped[fk_user] = mapped_column(index=True)
+    jti: Mapped[uid] = mapped_column(unique=True)
     issued_at: Mapped[datetime_now]
-    revoked: Mapped[bool_false]
     user_agent: Mapped[str_256 | None]
 
-    user: Mapped["Users"] = relationship(back_populates="jti")
+    user: Mapped["Users"] = relationship(back_populates="refresh_tokens")
