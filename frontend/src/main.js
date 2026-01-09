2b5da5804bf28bbb678b309233e29f9dfcefca42
Dmitry <dm1398@yandex.ru>
2026-01-09 12:53:51 +0300

    Корректировка импорта

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import App from './App.vue';
+import App from '@/App.vue';
 import { createPinia } from 'pinia';
 import { useAuthStore } from "@/stores/auth.js";
 import { useChatStore } from "@/stores/chat.js";
