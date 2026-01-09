bfa6892f1f5d7104692e863acaaa76199e63804f
Dmitry <dm1398@yandex.ru>
2026-01-09 21:49:52 +0300

    Добавлен PATCH /bot-settings

--- backend/app/api/app.py
+++ backend/app/api/app.py
@@ -36,8 +36,8 @@ async def get_bot_settings(db: DDB):
     """
 
     try:
-        data = await chatbot_settings.get_bot_settings(db)
-        return {**status_ok, "data": data}
+        result = await chatbot_settings.get_bot_settings(db)
+        return {**status_ok, "data": result}
 
     except BaseCustomException as ex:
         logger.exception(ex)
@@ -49,14 +49,33 @@ async def get_bot_settings(db: DDB):
 
 
 @app_router.put("/bot-settings", summary="Сохранение настроек бота", dependencies=[Depends(get_auth_admin_id)])
-async def save_bot_settings(data: SChatBotSettings):
+async def save_bot_settings_put(data: SChatBotSettings):
     """
-    Сохранение настроек чат-бота
+    Сохранение настроек чат-бота (PUT)
     """
 
     try:
-        data = await chatbot_settings.update_bot_settings(data)
-        return {**status_ok, "data": data}
+        await chatbot_settings.update_bot_settings(data)
+        return status_ok
+
+    except BaseCustomException as ex:
+        logger.exception(ex)
+        return ex.json_response
+
+    except (RuntimeError, Exception) as ex:
+        logger.exception(ex)
+        return http_error_500
+
+
+@app_router.patch("/bot-settings", summary="Сохранение настроек бота", dependencies=[Depends(get_auth_admin_id)])
+async def save_bot_settings_patch(data: SChatBotSettings):
+    """
+    Сохранение настроек чат-бота (PATCH)
+    """
+
+    try:
+        await chatbot_settings.update_bot_settings(data, exclude_unset=True)
+        return status_ok
 
     except BaseCustomException as ex:
         logger.exception(ex)
