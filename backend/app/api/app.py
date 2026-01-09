4e348656ccbe97b9c58c63c14db4f12164043569
Dmitry <dm1398@yandex.ru>
2026-01-09 13:06:49 +0300

    Добавлены маршруты получения и сохранения настроек чат-бота

--- backend/app/api/app.py
+++ backend/app/api/app.py
@@ -1,8 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
-from app.core import cache_manager
+from app.core import cache_manager, chatbot_settings
 from app.core.logs import logger
-from app.exceptions.api import settings_error
+from app.dependencies.auth import get_auth_admin_id
+from app.dependencies.db import DDB
+from app.exceptions.api import settings_error, http_error_500
+from app.exceptions.base import BaseCustomException
+from app.schemas.chatbot import SChatBotSettings
 from app.services.app import AppServices
 from app.types import status_ok
 
@@ -23,3 +27,41 @@ async def get_app_settings():
     except (AttributeError, TypeError, Exception) as ex:
         logger.exception(ex)
         return settings_error
+
+
+@app_router.get("/bot-settings", summary="Настройки бота", dependencies=[Depends(get_auth_admin_id)])
+async def get_bot_settings(db: DDB):
+    """
+    Получение настроек чат-бота
+    """
+
+    try:
+        data = await chatbot_settings.get_bot_settings(db)
+        return {**status_ok, "data": data}
+
+    except BaseCustomException as ex:
+        logger.exception(ex)
+        return ex.json_response
+
+    except (RuntimeError, Exception) as ex:
+        logger.exception(ex)
+        return http_error_500
+
+
+@app_router.put("/bot-settings", summary="Сохранение настроек бота", dependencies=[Depends(get_auth_admin_id)])
+async def save_bot_settings(data: SChatBotSettings):
+    """
+    Сохранение настроек чат-бота
+    """
+
+    try:
+        data = await chatbot_settings.update_bot_settings(data)
+        return {**status_ok, "data": data}
+
+    except BaseCustomException as ex:
+        logger.exception(ex)
+        return ex.json_response
+
+    except (RuntimeError, Exception) as ex:
+        logger.exception(ex)
+        return http_error_500
