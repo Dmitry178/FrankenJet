6d567e1747ec63219165501c4a6e2161635d3b6a
Dmitry <dm1398@yandex.ru>
2026-01-11 23:49:06 +0300

    Добавлен компонент отображения тегов статьи

---
+++ frontend/src/components/articles/ArticleTagsList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div v-if="tags && tags.length > 0" class="mt-4">
+    <h4 class="text-h6 mb-2">Теги</h4>
+    <div class="tag-chips-wrapper">
+      <v-chip
+        v-for="tag in tags"
+        :key="tag"
+        class="mr-2 mb-2 tag-chip"
+        size="small"
+        variant="outlined"
+        @click="$emit('tag-click', tag)"
+      >
+        {{ tag }}
+      </v-chip>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ArticleTagsList',
+  props: {
+    tags: Array,
+  },
+  emits: ['tag-click'],
+};
+</script>
+
+<style scoped>
+.tag-chips-wrapper {
+
+}
+.tag-chip {
+  cursor: pointer;
+}
+</style>
