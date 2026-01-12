961d702c5ac1e33083ace9d652232ba9213d34d2
Dmitry <dm1398@yandex.ru>
2026-01-12 23:15:00 +0300

    Добавлен компонент VMeta

---
+++ frontend/src/components/common/VMeta.vue
@@ -0,0 +1,15 @@
+<template>
+  <head>
+    <title>{{ title }}</title>
+    <meta name="description" :content="description">
+    <meta name="keywords" :content="keywords">
+  </head>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  title?: string;
+  description?: string;
+  keywords?: string;
+}>();
+</script>
