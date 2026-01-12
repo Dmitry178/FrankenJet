787f1eb55e850dff3d4cb6efac2d1a986e732e64
Dmitry <dm1398@yandex.ru>
2026-01-12 23:14:51 +0300

    Добавлен Breadcrumbs

---
+++ frontend/src/components/common/VBreadcrumbs.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-breadcrumbs :items="items">
+    <template v-slot:item="{ item }">
+      <v-breadcrumbs-item :href="item.href" :disabled="item.disabled">
+        {{ item.title }}
+      </v-breadcrumbs-item>
+    </template>
+  </v-breadcrumbs>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  items: Array<{ title: string; href: string; disabled?: boolean }>;
+}>();
+</script>
