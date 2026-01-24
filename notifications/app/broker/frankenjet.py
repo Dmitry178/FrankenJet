bf8a0dfb67a9fa364b63e4f11d087a82cee0fed2
Dmitry <dm1398@yandex.ru>
2026-01-24 12:53:03 +0300

    Переделана обработка сообщений от FrankenJet

--- notifications/app/broker/frankenjet.py
+++ notifications/app/broker/frankenjet.py
@@ -1,16 +1,17 @@
-import json
-
 from dataclasses import dataclass
 
 from app.bot import bot
 from app.bot.keyboards import get_admin_auth_keyboard, get_moderation_keyboard
-from app.bot.utils import prepare_expandable
+from app.bot.utils import prepare_expandable, send_logs_to_bot, send_notification_to_bot, send_message_to_admin
+from app.broker.utils import extract_broker_message
 from app.core.config import bot_settings
 from app.core.logs import bot_logger
 
+SERVICE_NAME = "Frankenjet"
+
 
 @dataclass
-class MsgTypes:
+class FJMsgTypes:
     """
     Типы сообщений на отправку в бот
     """
@@ -28,74 +29,37 @@ async def proceed_messages_from_frankenjet(message: str) -> None:
     """
 
     try:
-        json_data = json.loads(message)
-        msg_type = json_data.get("type")
-        msg_data = json_data.get("data")
-
-        if not msg_data:
-            raise ValueError
+        msg_type, msg_data = extract_broker_message(message)
 
         match msg_type:
 
-            case MsgTypes.log:
-                message_text = (
-                    f'⚠️ <b>Log Level:</b> {msg_data.get("level")}\n'
-                    f'📝 <b>Message:</b> {msg_data.get("message")}\n'
-                    f'📦 <b>Module:</b> {msg_data.get("module")}\n'
-                    f'🔧 <b>Function:</b> {msg_data.get("funcName")}\n'
-                    f'📄 <b>File:</b> {msg_data.get("module")}.py (line {msg_data.get("lineno")})\n'
-                    f'🕒 <b>Time:</b> {msg_data.get("asctime")}\n'
-                )
+            # отправка логов в бот
+            case FJMsgTypes.log:
+                await send_logs_to_bot(msg_data, SERVICE_NAME)
 
-                if msg_data.get("exc_text"):
-                    message_text += f'\n❌ <b>Exception:</b>\n<code>{msg_data.get("exc_text")}</code>\n'
+            # отправка технического уведомления в бот
+            case FJMsgTypes.info:
+                await send_notification_to_bot(msg_data)
 
-                if msg_data.get("stack_info"):
-                    message_text += f'\n🔍 <b>Stack Info:</b>\n<code>{msg_data.get("stack_info")}</code>\n'
+            # отправка уведомления в бот
+            case FJMsgTypes.notification:
+                await send_notification_to_bot(msg_data)
 
-                disable_notification = msg_data.get("level") in {"DEBUG", "INFO", "WARNING"}
-
-                await bot.send_message(
-                    chat_id=bot_settings.TELEGRAM_ADMIN_ID,
-                    text=message_text,
-                    disable_notification=disable_notification
-                )
-
-                if msg_data.get("traceback"):
-                    expandable_traceback = prepare_expandable(None, msg_data.get("traceback"))
-                    await bot.send_message(
-                        chat_id=bot_settings.TELEGRAM_ADMIN_ID,
-                        text=expandable_traceback,
-                        parse_mode="MarkdownV2"
-                    )
-
-            case MsgTypes.info:
-                # отправка технического уведомления в бот
-                message_text = f'<b>{msg_data.get("caption")}:</b> {msg_data.get("message")}'
-                await bot.send_message(
-                    chat_id=bot_settings.TELEGRAM_ADMIN_ID,
-                    text=message_text,
-                    disable_notification=True
-                )
-
-            case MsgTypes.notification:
-                # отправка уведомления в бот
-                await bot.send_message(chat_id=bot_settings.TELEGRAM_ADMIN_ID, text=msg_data)
-
-            case MsgTypes.auth_notification:
-                # сообщение об аутентификации администратора
+            # сообщение об аутентификации администратора
+            case FJMsgTypes.auth_notification:
+                user_id = msg_data.get("id")
+                user = msg_data.get("user", "")
                 message_text = (
                     "❗️<b>Произведён вход в приложение</b>\n"
-                    f'<b>Пользователь:</b> {msg_data.get("email")} (id: {msg_data.get("user-id")})\n'
-                    f'<b>IP:</b> {msg_data["client-ip"]}\n'
-                    f'<b>User Agent:</b> {msg_data["user-agent"]}'
+                    f'<b>Пользователь:</b> {user}\n'
+                    f'<b>IP:</b> {msg_data.get("client-ip", "")}\n'
+                    f'<b>User Agent:</b> {msg_data.get("user-agent", "")}'
                 )
-
-                keyboard = get_admin_auth_keyboard(msg_data.get("user-id"))
+                keyboard = get_admin_auth_keyboard(user_id, user)
                 await bot.send_message(chat_id=bot_settings.TELEGRAM_ADMIN_ID, text=message_text, reply_markup=keyboard)
 
-            case MsgTypes.moderation:
-                # модерация комментария
+            # модерация комментария
+            case FJMsgTypes.moderation:
                 expandable_comment = prepare_expandable("Модерация комментария", msg_data.get("comment"))
                 keyboard = get_moderation_keyboard(msg_data.get("id"))
                 await bot.send_message(
@@ -106,9 +70,14 @@ async def proceed_messages_from_frankenjet(message: str) -> None:
                 )
 
             case _:
-                bot_logger.error(f'Ошибка типа сообщения "{msg_type}"')
+                msg = f'Ошибка типа сообщения "{msg_type}"'
+                bot_logger.error(msg)
+                await send_message_to_admin(f"⚠️ {msg}\n{message}")
 
     except (Exception, ValueError) as ex:
-        message = "Ошибка обработки сообщения из RabbitMQ"
-        bot_logger.exception(f"{message}: {ex}")
-        await bot.send_message(f"⚠️ {message}")
+        msg = "Ошибка обработки сообщения из RabbitMQ"
+        bot_logger.exception(msg, extra={"error": str(ex)})
+        err_msg = prepare_expandable(f"⚠️ {msg}", str(ex))
+        await send_message_to_admin(err_msg)
+
+    return None
