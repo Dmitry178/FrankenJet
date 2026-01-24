44fe289687d4b95cb239031ecef1c29e5c3abf94
Dmitry <dm1398@yandex.ru>
2026-01-24 12:58:48 +0300

    Добавлена подписка на сообщения от Vectorizer

--- notifications/app/broker/subscribers.py
+++ notifications/app/broker/subscribers.py
@@ -3,7 +3,8 @@
 from faststream.rabbit import RabbitBroker
 
 from app.broker.frankenjet import proceed_messages_from_frankenjet
-from app.core.config import bot_settings, RMQ_FJ_OUTPUT_QUEUE
+from app.broker.vectorizer import proceed_messages_from_vectorizer
+from app.core.config import bot_settings, RMQ_FJ_OUTPUT_QUEUE, RMQ_VECTORIZER_QUEUE
 
 broker = RabbitBroker(
     url=bot_settings.RMQ_CONN,
@@ -11,9 +12,18 @@ broker = RabbitBroker(
 
 
 @broker.subscriber(RMQ_FJ_OUTPUT_QUEUE)
-async def handle_frankenjet(message: str):
+async def handle_frankenjet(data: str):
     """
-    Получение сообщений от FrankenJet
+    Подписка на сообщения от Franken Jet
     """
 
-    await proceed_messages_from_frankenjet(message)
+    await proceed_messages_from_frankenjet(data)
+
+
+@broker.subscriber(RMQ_VECTORIZER_QUEUE)
+async def handle_vectorizer(data: str):
+    """
+    Подписка на сообщения от Vectorizer
+    """
+
+    await proceed_messages_from_vectorizer(data)
